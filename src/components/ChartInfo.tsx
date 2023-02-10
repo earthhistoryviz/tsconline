aca88cf9c1986bede5c94fee62f782eb34cdeac2
Aaron Ault <aultac@gmail.com>
2023-02-10 13:32:12 -0500

    After weekly meeting

--- src/components/ChartInfo.tsx
+++ src/components/ChartInfo.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import {observer} from "mobx-react-lite";
+import imgSrc from '../assets/AfricaBight_Nigeria_Image.jpg'
 import "./chartinfo.css"
 
 export interface ChartBlock {
@@ -11,7 +12,7 @@ export interface ChartBlock {
 export default observer(function ChartInfo(chart: ChartBlock) {
         return (
         <div className="panel">
-            <img style={{width: "375px", height: "250px"}} src={chart.imageSrc}></img>
+            <img style={{width: "375px", height: "250px"}} src={imgSrc} />
             <h1 className="header1">{chart.dataPackTitle}</h1>
             <h2 className="header2">{chart.dataPackDescription}</h2>
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,52 +9,35 @@ import "./Home.css"
 import {Chart} from './Chart';
 
 
-export function Home() {
+export const Home = observer(function () {
     const { state, actions } = useContext(context); 
-    const charts: Array<ChartBlock> = [];
-
-    for (let i = 0; i < 10; i++) {
-        let chart: ChartBlock = {
-            imageSrc: imgSrc,
-            dataPackTitle: 'AfricaBight_Nigeria_Image Maps ' + i,
-            dataPackDescription: 'data pack number ' + i,
-            chartNumber: i,
-        };
-
-        charts.push(chart)
 
     const handleClick = useCallback(() => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }, []);
 
-    }
-                //{ chartToDisplay = charts.at(state.chart)!  
-    let chartToDisplay: ChartBlock = charts.at(state.chart)!;
-            //
     return (
 
-            <div className="panel">
+      <div className="panel">
          <div style={{position: "relative", width: "1350px", height: "300px", outline: "4px solid blue"}}>
             <div className="chart_display">
-                <img style={{width: "450px", height: "300px"}} src={charts.at(state.chart).imageSrc}></img>
+                <img style={{width: "450px", height: "300px"}} src={imgSrc}></img>
                 <div className="details" >
-                    <h2 style={{ marginLeft: 100, marginRight: 50 }}>Preset Title</h2>
+                    <h2 style={{ marginLeft: 100, marginRight: 50 }}>Preset Title: chart number is {state.chart.chartNumber} </h2>
                     <p className="description" style={{ marginLeft: 100, marginRight: 50 }}>description of the chart description of the chart description of the chart description of the chart description of the chart description of the chart description of the chart description of the chart description of the chart description of the chart description of the chart </p>
                     <img style={{marginLeft: 220, width: "450px", height: "100px"}} src={imgSrc}></img>
                 </div>
             </div>
-        <div className="options">
-            {...charts.map((chart) => (
-                <div className="item" onClick={() => {
-                actions.setChart(chart.chartNumber)
-                
-
+            <div className="options">
+              {state.charts.map((chart, index) => (
+                <div key={`chart_${index}`} className="item" onClick={() => {
+                  actions.setChart(chart.chartNumber)
                 }}>
-                    <ChartInfo{...chart} />
+                  <ChartInfo {...chart} />
                 </div>
-            ))}
-        </div>
-        </div>
+              ))}
+            </div>
+          </div>
         </div>
     )
-}
+});
