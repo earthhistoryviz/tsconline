8c4a10c4cb801f90e7149e7a5c5f67ddc13dcfe6
Piper Hale <pijohale16@gmail.com>
2023-02-17 12:19:22 -0500

    undoing test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ export default observer(function Navbar() {
                 </Link>
                 { (!state.showAllTabs) 
                 ? 
-                  <div onClick={() => actions.setTab(1)}>There are no tabs piper change</div> 
+                  <div onClick={() => actions.setTab(1)}>There are no tabs</div> 
                 : 
                   <Tabs 
                   textColor="inherit" 
