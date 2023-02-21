82d9c1e60521f3b021e7f53eb0dfb82235cfdbed
Mahad Faruqi <mahadkhalid4955@gmail.com>
2023-02-20 21:10:41 -0500

    forgot to git add server files

---
+++ server/dist/index.js
@@ -0,0 +1,15 @@
+import fastify from 'fastify';
+const server = fastify();
+console.log('here');
+console.log('here');
+server.get('/ping', async (request, reply) => {
+    return 'pong\n';
+});
+server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
+//# sourceMappingURL=index.js.map
