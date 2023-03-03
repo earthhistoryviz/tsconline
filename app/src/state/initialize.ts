d02c1baf552c60870b8bb2ff6e080b9ddf562529
Mahad Faruqi <mahadkhalid4955@gmail.com>
2023-03-03 11:52:14 -0500

    can load charts from json file now

--- app/src/state/initialize.ts
+++ app/src/state/initialize.ts
@@ -2,21 +2,34 @@
 //var fs = require('fs');
 import { useCallback, useContext } from 'react'
 import { observer } from 'mobx-react-lite';
-import { context } from '../state';
-import {loadCharts} from './loadCharts'
+import { context } from './index';
+import { loadCharts } from './loadCharts'
+import { actions } from './index'
 
-
-import axios from 'axios';
+import { state, State } from './state';
 
 export async function initialize() {
+
+   //const { state, actions } = useContext(context);
+
   // put any initialization code here
   console.log('here at initialize');
   await fetch('http://localhost:3000/charts',
               {"method": 'POST'})
     .then(res => res.json()) 
     .then(response =>  {
+      console.log(response) 
+      let charts = response.charts;
+      actions.loadCharts(charts);
+      for (let index in charts) {
+        console.log(index);
+      }
+    });
+    /*
+    .then(response =>  {
       loadCharts(response);
     });
+    */
 }
 
   /*
