52b636fb789ea23f876a1f2dce27d62ebb0c98ff
siyaduttsharma <67598486+siyaduttsharma@users.noreply.github.com>
2023-08-02 10:54:40 -0400

    Update Settings.tsx

--- app/src/Settings.tsx
+++ app/src/Settings.tsx
@@ -12,17 +12,18 @@ export function Settings() {
 
   const [topAge, setTopAge] = useState(state.settings.topAge);
   const [baseAge, setBaseAge] = useState(state.settings.baseAge);
-  const [verticalScale, setVerticalScale] = useState(state.settings.verticalScale);
+  const [unitsPerMY, setUnitsPerMY] = useState(state.settings.unitsPerMY);
 
   state.settings.topAge = topAge;
   state.settings.baseAge = baseAge;
+  state.settings.unitsPerMY = unitsPerMY;
 
   const handleButtonClick = () => {
     actions.setTab(1);
     actions.setAllTabs(true);
   
     // Validate the user input
-    if (isNaN(topAge) || isNaN(baseAge) || isNaN(verticalScale)) {
+    if (isNaN(topAge) || isNaN(baseAge) || isNaN(unitsPerMY)) {
       // Handle invalid input, show error message, etc.
       return;
     }
@@ -66,14 +67,14 @@ export function Settings() {
       <TextField
         label="Vertical Scale (cm/Ma)"
         type="number"
-        value={verticalScale}
-        onChange={(event) => setVerticalScale(parseFloat(event.target.value))}
+        value={unitsPerMY}
+        onChange={(event) => setUnitsPerMY(parseFloat(event.target.value))}
         style={{ marginBottom: '20px', width: '100%' }}
       />
       {settingOptions.map(({ name, label, stateName }) => (
         <FormControlLabel
           key={name}
-          control={<Checkbox checked={state.settingsJSON[stateName]} onChange={(event) => updateCheckboxSetting(stateName, event.target.checked)} />}
+          control={<Checkbox checked={state.settings[stateName]} onChange={(event) => updateCheckboxSetting(stateName, event.target.checked)} />}
           label={label}
           style={{ marginBottom: '10px' }} 
         />
