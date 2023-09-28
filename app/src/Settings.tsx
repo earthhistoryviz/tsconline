d89d572d9c786b01081aef5d93ad04a0f327d2d9
jay-jhk-lee <jaylee.62893@gmail.com>
2023-09-28 16:14:58 -0400

    added navigation bar for settings

--- app/src/Settings.tsx
+++ app/src/Settings.tsx
@@ -1,16 +1,51 @@
 import React, { useContext } from 'react';
-import { Button, TextField, Box, FormControlLabel, Checkbox } from '@mui/material';
+import { Button, TextField, Box, FormControlLabel, Checkbox, Tabs, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import ForwardIcon from '@mui/icons-material/Forward';
 import { context } from './state';
 import { primary_dark } from './constant';
 import { settingOptions, updateCheckboxSetting } from './state/actions';
 import { observer } from 'mobx-react-lite';
+import Tab from '@mui/material/Tab';
+import { Column } from './SettingsTabs/Column';
+import { Time } from './SettingsTabs/Time';
+import { Font } from './SettingsTabs/Font';
+import { MapPoint } from './SettingsTabs/MapPoint';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 export const Settings = observer(function Settings() {
   const { state, actions } = useContext(context);
   const navigate = useNavigate();
+  const [value, setValue] = React.useState(0);
 
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
   const handleButtonClick = () => {
     actions.setTab(1);
     actions.setAllTabs(true);
@@ -32,7 +67,25 @@ console.log('RENDER: state = ', state.settings);
 
   return (
     <div>
-      <div>A navbar should go here</div>
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label="Time" />
+        <Tab label="Column"/>
+        <Tab label="Font"/>
+        <Tab label="Map Points"/>
+      </Tabs>
+      <CustomTabPanel value={value} index={0}>
+        <Time></Time>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+      <Column></Column>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <Font></Font>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={3}>
+        <MapPoint></MapPoint>
+      </CustomTabPanel>
+      <div></div>
       <Box
         display="flex"
         flexDirection="column"
---
+++ app/src/SettingsTabs/Time.tsx
@@ -0,0 +1,9 @@
+import { observer } from "mobx-react-lite";
+import React from "react";
+export const Time = observer(function Time() {
+    return (
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>This is Time</div>
+    )
+}
+
+)
