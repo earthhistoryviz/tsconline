f8583ea4e34b6b3bd3e7cd45af748b163ce26871
paologum <pgumasin@purdue.edu>
2023-12-25 22:55:01 -0500

    fixed node packages

--- app/src/util.ts
+++ app/src/util.ts
@@ -15,20 +15,16 @@ export function devSafeUrl(url: string) {
   // browser from the node server instead of the vite dev server), then it 
   // will be false.
   if (import.meta.env.DEV) {
-    console.log(devurl + url)
     return devurl + url;
   }
-  console.log(url)
   return url;
 }
 
 export async function fetcher(...args: Parameters<typeof fetch>): ReturnType<typeof fetch> {
-  // if (import.meta.env.DEV) {
-  //   if (typeof args[0] === 'string') { 
-  //     args[0] = devSafeUrl(args[0]);
-  //   }
-  // }
-  args[0] = "http://dev.timescalecreator.org:3000"
-  console.log(args[0])
+  if (import.meta.env.DEV) {
+    if (typeof args[0] === 'string') { 
+      args[0] = devSafeUrl(args[0]);
+    }
+  }
   return fetch(...args);
 }
