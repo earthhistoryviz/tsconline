e2303d249777304dffbb48a1f5c4f43d8bb32427
paologum <pgumasin@purdue.edu>
2023-12-31 14:55:54 -0600

    added map points type

--- shared/dist/index.js
+++ shared/dist/index.js
@@ -1,31 +1,60 @@
 // Shared types between app and server (i.e. messages they send back and forth)
+export function assertMapPoints(o) {
+    if (typeof o !== 'object' || o === null) {
+        throw new Error('MapPoints must be a non-null object');
+    }
+    for (const key in o) {
+        const point = o[key];
+        if (typeof point !== 'object' || point === null) {
+            throw new Error(`MapPoints' value for key '${key}' must be a non-null object`);
+        }
+        if (typeof point.lat !== 'number') {
+            throw new Error(`MapPoints' value for key '${key}' must have a 'lat' number property`);
+        }
+        if (typeof point.lon !== 'number') {
+            throw new Error(`MapPoints' value for key '${key}' must have a 'lon' number property`);
+        }
+        if (point.default !== undefined && typeof point.default !== 'string') {
+            throw new Error(`MapPoints' value for key '${key}' must have a 'default' string property`);
+        }
+        if (point.minage !== undefined && typeof point.minage !== 'number') {
+            throw new Error(`MapPoints' value for key '${key}' must have a 'minage' number property`);
+        }
+        if (point.maxage !== undefined && typeof point.maxage !== 'number') {
+            throw new Error(`MapPoints' value for key '${key}' must have a 'maxage' number property`);
+        }
+        if (point.note !== undefined && typeof point.note !== 'string') {
+            throw new Error(`MapPoints' value for key '${key}' must have a 'note' string property`);
+        }
+    }
+}
 export function assertChartConfig(o) {
     if (typeof o !== 'object')
-        throw 'ChartConfig must be an object';
+        throw new Error('ChartConfig must be an object');
     if (typeof o.img !== 'string')
-        throw 'ChartConfig must have an img string';
+        throw new Error('ChartConfig must have an img string');
     if (typeof o.title !== 'string')
-        throw 'ChartConfig must have a title string';
+        throw new Error('ChartConfig must have a title string');
     if (typeof o.description !== 'string')
-        throw 'ChartConfig must have a description string';
+        throw new Error('ChartConfig must have a description string');
     if (typeof o.settings !== 'string')
-        throw 'ChartConfig must have a settings path string';
+        throw new Error('ChartConfig must have a settings path string');
     if (!Array.isArray(o.datapacks))
-        throw 'ChartConfig must have a datapacks array of datapack string names.  ';
+        throw new Error('ChartConfig must have a datapacks array of datapack string names.  ');
 }
 export function assertChartConfigArray(o) {
     if (!o || !Array.isArray(o))
-        throw 'ChartConfig array must be an array';
+        throw new Error('ChartConfig array must be an array');
     for (const c of o)
         assertChartConfig(c);
 }
 export function assertChartRequest(o) {
     if (typeof o !== 'object')
-        throw 'ChartRequest must be an object';
+        throw new Error('ChartRequest must be an object');
     if (typeof o.settings !== 'string')
-        throw 'ChartRequest must have a settings string';
+        throw new Error('ChartRequest must have a settings string');
     if (!Array.isArray(o.datapacks))
-        throw 'ChartRequest must have a datapacks array';
+        throw new Error('ChartRequest must have a datapacks array');
 }
 export function isChartError(o) {
     if (!o || typeof o !== 'object')
@@ -36,8 +65,10 @@ export function isChartError(o) {
 }
 export function assertChartInfo(o) {
     if (!o || typeof o !== 'object')
-        throw 'ChartInfo must be an object';
+        throw new Error('ChartInfo must be an object');
     if (typeof o.chartpath !== 'string')
-        throw 'ChartInfo must have a chartpath string';
+        throw new Error('ChartInfo must have a chartpath string');
+    if (typeof o.hash !== 'string')
+        throw new Error('ChartInfo must have a hash string');
 }
 //# sourceMappingURL=index.js.map
