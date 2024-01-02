f57178e1c8539c2b87629b4894f2ab66ec73bae9
paologum <pgumasin@purdue.edu>
2024-01-01 16:03:13 -0800

    finished raw version of map points

--- app/src/SettingsTabs/MapPoint.tsx
+++ app/src/SettingsTabs/MapPoint.tsx
@@ -1,17 +1,17 @@
 import { observer } from "mobx-react-lite";
 import { useContext } from "react";
 import { context } from "../state";
-import { ImageRowComponent } from "../assets"
+import { MapRowComponent } from "../assets"
 export const MapPoint = observer(function MapPoint() {
     const { state } = useContext(context);
     return (
-        <div style={{ height: '500px', width: '100%', position: 'relative' }}>
+        <div>
             { !state.settingsTabs.mapImages || state.settingsTabs.mapImages.length === 0 ?
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
                     This is Map Point
                 </div>
                 :
-                <ImageRowComponent maps={state.settingsTabs.maps}/>
+                <MapRowComponent maps={state.settingsTabs.maps}/>
         }
         </div>
     )
