d642d6a25865fb8c41c16434a25c5ea172991c5a
paologum <pgumasin@purdue.edu>
2024-01-11 04:21:47 -0500

    updated docs up to server

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { context, initialContext } from './state';
-import './main.css'
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
