1db99ec0adbe48305ad58bfc6c4e2023e4a4c1fb
Paolo Gumasing <pgumasin@purdue.edu>
2024-01-24 11:55:07 -0500

    white input text field added to component

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,6 +1,7 @@
 export { TSCCheckbox } from './TSCCheckbox';
 export { TSCButton } from './TSCButton';
 export { TSCTab, TSCTabs } from './TSCTabs';
+export { TSCTextField } from './TSCTextField';
 export { TSCCardList } from './TSCCardList';
 export { TSCIcon } from './TSCIcon';
 export { Accordion, AccordionSummary, AccordionDetails, ColumnContainer} from './TSCColumns';
