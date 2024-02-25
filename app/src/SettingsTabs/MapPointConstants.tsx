3ccd36e4b70527aee2cf67bb23b9ba73a2e84f94
Paolo Gumasing <pgumasin@purdue.edu>
2024-02-24 23:56:33 -0500

    final styling to make facies options work on mobile and desktop
    fix decimals in slider

--- app/src/SettingsTabs/MapPointConstants.tsx
+++ app/src/SettingsTabs/MapPointConstants.tsx
@@ -1,4 +1,4 @@
 // height of header bar without the controls
-export const normHeaderHeight = "8vh";
+export const normHeaderHeight = "10vh";
 // this is to account for the added controls
-export const faciesHeaderHeight = "17vh";
+export const faciesHeaderHeight = "20vh";
