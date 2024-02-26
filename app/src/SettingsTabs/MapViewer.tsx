4ab26fdbe1835bf4914b524f1714cf7e48acdb73
paologum <pgumasin@purdue.edu>
2024-02-26 11:01:16 -0500

    eslint fixes

--- app/src/SettingsTabs/MapViewer.tsx
+++ app/src/SettingsTabs/MapViewer.tsx
@@ -1,11 +1,11 @@
-import { Drawer, Divider, IconButton, Slider } from "@mui/material";
+import { Drawer, Divider, IconButton } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import type { MapHierarchy, MapInfo } from "@tsconline/shared";
 import { devSafeUrl } from "../util";
 import React, { useEffect, useState, useRef, useContext } from "react";
 import { context } from "../state";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import { DrawerHeader, ColoredIconButton, TypographyText, BorderedIcon, ColoredDiv } from "../components";
+import { DrawerHeader, ColoredIconButton, TypographyText, BorderedIcon } from "../components";
 import CloseIcon from "@mui/icons-material/Close";
 import FullscreenIcon from "@mui/icons-material/Fullscreen";
 import ZoomInIcon from "@mui/icons-material/ZoomIn";
