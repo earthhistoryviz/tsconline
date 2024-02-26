48595a397b144651941cc670d9dd8490cbb4193d
jay-jhk-lee <jaylee.62893@gmail.com>
2024-02-26 00:52:37 -0500

    some possible types for children column

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -82,21 +82,23 @@ export type Datapack = {
   file: string;
 };
 
-export type ChartInfo = {
-  settings: ChartSettingsInfo;
-  "class datastore.RootColumn:Chart Root": [name: ColumnPrototypeInfo];
+export type PresetChartInfo = {
+  settings: PresetChartSettingsInfo;
+  "class datastore.RootColumn:Chart Root": PresetColumnInfo;
 };
 
-export type ChartSettingsInfo = {
+export type PresetChartSettingsInfo = {
   topAge: {
     source: string;
     unit: string;
-    text: number;
+    stage?: string;
+    text?: number;
   };
   baseAge: {
     source: string;
     unit: string;
-    text: number;
+    stage?: string;
+    text?: number;
   };
   unitsPerMY: {
     unit: string;
@@ -116,7 +118,7 @@ export type ChartSettingsInfo = {
   enHideBlockLable: boolean;
 };
 
-export type ColumnPrototypeInfo = {
+export type PresetColumnInfo = {
   _id: string;
   title: string;
   useNamedColor: boolean;
@@ -129,10 +131,19 @@ export type ColumnPrototypeInfo = {
   pad: number;
   "age pad": number;
   backgroundColor: {
-    useNamed: boolean;
+    standardized?: boolean;
+    useNamed?: boolean;
     text: string;
   };
   fonts: FontsInfo;
+  //simplest, to access by name have to iterate through the array
+  children: PresetColumnInfo[];
+  //access by name good, ordering isn't maintained, can't access by index
+  children2: { [name: string]: PresetColumnInfo };
+  //access by index good, access by name = iterate, maintain order
+  children3: [[name: string, column: PresetColumnInfo]];
+  //access by index good, access by name = iterate, maintain order
+  children4: [{ name: string; column: PresetColumnInfo }];
 };
 
 export type FontsInfo = {
@@ -220,16 +231,23 @@ export const defaultFontsInfo: FontsInfo = {
     fontFace: "Arial",
     inheritable: false,
     italic: false,
-    size: 6
+    size: 6,
   },
-  "Column Header": { bold: false, color: "#000000", fontFace: "Arial", inheritable: false, italic: false, size: 14 },
+  "Column Header": {
+    bold: false,
+    color: "#000000",
+    fontFace: "Arial",
+    inheritable: false,
+    italic: false,
+    size: 14,
+  },
   "Event Column Label": {
     bold: false,
     color: "#000000",
     fontFace: "Arial",
     inheritable: false,
     italic: false,
-    size: 11
+    size: 11,
   },
   "Legend Column Name": { inheritable: false },
   "Legend Column Source": { inheritable: false },
@@ -237,12 +255,33 @@ export const defaultFontsInfo: FontsInfo = {
   "Point Column Scale Label": { inheritable: false },
   "Popup Body": { inheritable: false },
   "Range Box Label": { inheritable: false },
-  "Range Label": { bold: false, color: "#000000", fontFace: "Arial", inheritable: false, italic: false, size: 12 },
+  "Range Label": {
+    bold: false,
+    color: "#000000",
+    fontFace: "Arial",
+    inheritable: false,
+    italic: false,
+    size: 12,
+  },
   "Ruler Label": { inheritable: false },
   "Ruler Tick Mark Label": { inheritable: false },
   "Sequence Column Label": { inheritable: false },
-  "Uncertainty Label": { bold: false, color: "#000000", fontFace: "Arial", inheritable: false, italic: false, size: 5 },
-  "Zone Column Label": { bold: false, color: "#000000", fontFace: "Arial", inheritable: false, italic: false, size: 12 }
+  "Uncertainty Label": {
+    bold: false,
+    color: "#000000",
+    fontFace: "Arial",
+    inheritable: false,
+    italic: false,
+    size: 5,
+  },
+  "Zone Column Label": {
+    bold: false,
+    color: "#000000",
+    fontFace: "Arial",
+    inheritable: false,
+    italic: false,
+    size: 12,
+  },
 };
 
 export type SubBlockInfo = {
