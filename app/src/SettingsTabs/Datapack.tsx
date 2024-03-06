e4970abea3eb6004afec4cf4eb7c8e38f46fec84
Rebecca <rupp3@purdue.edu>
2024-03-06 01:37:02 -0600

    checkboxes add datapacks to columns in subtab

--- app/src/SettingsTabs/Datapack.tsx
+++ app/src/SettingsTabs/Datapack.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { useTheme } from "@mui/material/styles";
 import { TSCCheckbox, TSCDatapackUploadForm, TSCButton } from "../components";
+import { context, state } from "../state";
 import Box from "@mui/material/Box";
 import InfoIcon from "@mui/icons-material/Info";
 import Tooltip from "@mui/material/Tooltip";
@@ -12,36 +13,17 @@ import { Dialog } from "@mui/material";
 
 export const Datapacks = observer(function Datapacks() {
   const theme = useTheme();
+  const { actions } = useContext(context);
   const [formOpen, setFormOpen] = useState(false);
 
-  // dummy datapacks data
-  //DatapackParsingPack in shared for the real datapacks
-  const datapacks = [
-    {
-      name: "British Isles",
-      description:
-        "British Isles regional formations with map interface. Approximately two dozen sub-regions, with each formation linked to the BGS lexicon; compiled in coordination with British Geological Survey, BGS."
-    },
-    {
-      name: "Belgium",
-      description:
-        "Belgium regional formations with map interface. Detailed Table of contents and references at Belgium_LithostratContents.pdf. Compiled by Jane Block in coordination with Belgium Stratigraphic Commission (Noel Vandenberghe, Hance Luc, et al.)."
-    },
-    { name: "Australia", description: "Data for Australia" },
-    {
-      name: "Very Long Datapack Name _________________ Example_______________________",
-      description: "Data for Longname"
-    }
-  ];
-
-  const [selectedDatapacks, setSelectedDatapacks] = useState<string[]>([]);
-
   const handleCheckboxChange = (name: string) => {
-    const isSelected = selectedDatapacks.includes(name);
-    if (isSelected) {
-      setSelectedDatapacks(selectedDatapacks.filter((selectedName) => selectedName !== name));
+    if (state.config.datapacks.includes(name)) {
+      actions.setDatapackConfig(
+        state.config.datapacks.filter((datapack) => datapack !== name),
+        ""
+      );
     } else {
-      setSelectedDatapacks([...selectedDatapacks, name]);
+      actions.setDatapackConfig([...state.config.datapacks, name], "");
     }
   };
 
@@ -55,22 +37,22 @@ export const Datapacks = observer(function Datapacks() {
         <Box className="box-container">
           <table className="data-table">
             <tbody>
-              {datapacks.map((datapack) => (
-                <tr key={datapack.name}>
+              {Object.keys(state.datapackIndex).map((datapack, index) => (
+                <tr key={index}>
                   <td className="checkbox-cell">
                     <TSCCheckbox
-                      checked={selectedDatapacks.includes(datapack.name)}
-                      onChange={() => handleCheckboxChange(datapack.name)}
+                      checked={state.config.datapacks.includes(datapack)}
+                      onChange={() => handleCheckboxChange(datapack)}
                     />
                   </td>
                   <td>
                     <div className="name-cell">
-                      <Typography>{datapack.name}</Typography>
+                      <Typography>{datapack}</Typography>
                     </div>
                   </td>
                   <td className="info-cell">
                     <div>
-                      <Tooltip title={datapack.description} arrow placement="right">
+                      <Tooltip title="Description" arrow placement="right">
                         <InfoIcon className="info-icon" />
                       </Tooltip>
                     </div>
