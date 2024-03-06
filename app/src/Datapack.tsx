60838fcf7b3e6a04e286ef9cf824461161794467
Rebecca <rupp3@purdue.edu>
2024-03-06 01:37:02 -0600

    using presets to test datapack list
    NOT WORKING, went back to setup
    
    datapacks from repo display, using apply button to test
    
    datapacks display, using apply button to test

--- app/src/Datapack.tsx
+++ app/src/Datapack.tsx
@@ -1,9 +1,9 @@
-import { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import { useTheme } from "@mui/material/styles";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import { TSCCheckbox, InputFileUpload } from "./components";
-import { context } from "./state";
+import { TSCCheckbox, InputFileUpload ,TSCButton } from "./components";
+import { context, state } from "./state";
 import Box from "@mui/material/Box";
 import InfoIcon from "@mui/icons-material/Info";
 import Tooltip from "@mui/material/Tooltip";
@@ -14,36 +14,22 @@ import "./Datapack.css";
 export const Datapack = observer(function Datapack() {
   const theme = useTheme();
   const { actions } = useContext(context);
-
-  // dummy datapacks data
-  //DatapackParsingPack in shared for the real datapacks
-  const datapacks = [
-    {
-      name: "British Isles",
-      description:
-        "British Isles regional formations with map interface. Approximately two dozen sub-regions, with each formation linked to the BGS lexicon; compiled in coordination with British Geological Survey, BGS."
-    },
-    {
-      name: "Belgium",
-      description:
-        "Belgium regional formations with map interface. Detailed Table of contents and references at Belgium_LithostratContents.pdf. Compiled by Jane Block in coordination with Belgium Stratigraphic Commission (Noel Vandenberghe, Hance Luc, et al.)."
-    },
-    { name: "Australia", description: "Data for Australia" },
-    {
-      name: "Very Long Datapack Name _________________ Example_______________________",
-      description: "Data for Longname"
-    }
-  ];
-
   const [selectedDatapacks, setSelectedDatapacks] = useState<string[]>([]);
+  const datapackIndex = state.datapackIndex;
+
+  useEffect(() => {
+  }, []);
 
   const handleCheckboxChange = (name: string) => {
-    const isSelected = selectedDatapacks.includes(name);
-    if (isSelected) {
-      setSelectedDatapacks(selectedDatapacks.filter((selectedName) => selectedName !== name));
+    if (selectedDatapacks.includes(name)) {
+      setSelectedDatapacks(selectedDatapacks.filter(selectedName => selectedName !== name));
     } else {
       setSelectedDatapacks([...selectedDatapacks, name]);
     }
+  };
+
+  const setDatapackConfigAndUpdate = () => {
+    actions.setDatapackConfig(selectedDatapacks, state.config.settingsPath);
   };
 
   return (
@@ -56,22 +42,22 @@ export const Datapack = observer(function Datapack() {
         <Box className="box-container">
           <table className="data-table">
             <tbody>
-              {datapacks.map((datapack) => (
-                <tr key={datapack.name}>
+              {Object.keys(datapackIndex).map((datapack, index) => (
+                <tr key={index}>
                   <td className="checkbox-cell">
                     <TSCCheckbox
-                      checked={selectedDatapacks.includes(datapack.name)}
-                      onChange={() => handleCheckboxChange(datapack.name)}
+                      checked={selectedDatapacks.includes(datapack)}
+                      onChange={() => handleCheckboxChange(datapack)}
                     />
                   </td>
                   <td>
                     <div className="name-cell">
-                      <Typography>{datapack.name}</Typography>
+                      <Typography>{datapack}</Typography>
                     </div>
                   </td>
                   <td className="info-cell">
                     <div>
-                      <Tooltip title={datapack.description} arrow placement="right">
+                      <Tooltip title="Description" arrow placement="right">
                         <InfoIcon className="info-icon" />
                       </Tooltip>
                     </div>
@@ -81,6 +67,7 @@ export const Datapack = observer(function Datapack() {
             </tbody>
           </table>
         </Box>
+        <TSCButton onClick={setDatapackConfigAndUpdate}>Apply Selected Datapacks</TSCButton>
 
         <InputFileUpload
           startIcon={<CloudUploadIcon />}
@@ -90,6 +77,7 @@ export const Datapack = observer(function Datapack() {
             actions.uploadDatapack(file);
           }}
         />
+
       </div>
     </div>
   );
