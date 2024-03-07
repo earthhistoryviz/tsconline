35cecf74efaabc2cc903e63c369c063ec18d5cd0
paologum <pgumasin@purdue.edu>
2024-03-06 21:31:37 -0500

    revert hover

--- app/src/NavBar.tsx
+++ app/src/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useContext, useRef } from "react";
+import { useContext } from "react";
 import { observer } from "mobx-react-lite";
 import AppBar from "@mui/material/AppBar";
 import { Link } from "react-router-dom";
@@ -9,18 +9,12 @@ import HomeIcon from "@mui/icons-material/Home";
 import { IconButton, Tab } from "@mui/material";
 import { context } from "./state";
 import { TSCTabs } from "./components";
-import "@szhsin/react-menu/dist/index.css";
-import "@szhsin/react-menu/dist/transitions/slide.css";
-import { ControlledMenu, MenuDivider, MenuItem, SubMenu, useClick, useHover, useMenuState } from "@szhsin/react-menu";
 
 import "./NavBar.css";
 
 export const NavBar = observer(function Navbar() {
   const theme = useTheme();
   const { state, actions } = useContext(context);
-  const menuRef= useRef(null);
-  const [menuState, toggle] = useMenuState({ transition: true });
-  const { anchorProps, hoverProps } = useHover(menuState.state, toggle);
   return (
     <AppBar position="fixed" sx={{ background: theme.palette.navbar.main, display: "flex" }}>
       <Toolbar>
@@ -61,24 +55,14 @@ export const NavBar = observer(function Navbar() {
               }
             }}>
             <Tab value={1} label="Chart" to="/chart" component={Link} />
-            <Tab value={2} label="Settings" to="/settings" component={Link}  ref={menuRef} {...anchorProps}/>
-            <Tab value={3} label="Datapack" to="/datapack" component={Link}/>
+            <Tab value={2} label="Settings" to="/settings" component={Link} />
+            <Tab value={3} label="Datapack" to="/datapack" component={Link} />
             <Tab value={4} label="Help" to="/help" component={Link} />
             <Tab value={5} label="About" to="/about" component={Link} />
           </TSCTabs>
         }
         <div style={{ flexGrow: 1 }} />
         <img src={TSCreatorLogo} width="4%" height="4%" />
-        <ControlledMenu
-          {...hoverProps}
-          {...menuState}
-          anchorRef={menuRef}
-          onClose={() => toggle(false)}
-        >
-          <MenuItem>Cut</MenuItem>
-          <MenuItem>Copy</MenuItem>
-          <MenuItem>Paste</MenuItem>
-        </ControlledMenu>
       </Toolbar>
     </AppBar>
   );
