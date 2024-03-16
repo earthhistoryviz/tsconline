dc496eea3b0e9a23427c48884d3beede0601a5fc
paologum <pgumasin@purdue.edu>
2024-03-16 04:17:23 +0100

    deprecated pushError useless message function

--- app/src/state/actions/util-actions.ts
+++ app/src/state/actions/util-actions.ts
@@ -1,20 +1,21 @@
+import { ErrorCodes } from "../../util/error-codes";
 import { pushError } from "./general-actions";
 import { isServerResponseError } from "@tsconline/shared";
 
 /**
- * Display error to dialog popup
+ * Display error to dialog popup, same as pushError but with a message logged to console (Use this for server response errors)
  * @param error the error thrown
  * @param response the response from the server if applicable (nullable)
  * @param message the message to be shown
  */
-export function displayServerError<T>(response: T | null, context: string, message: string) {
+export function displayServerError<T>(response: T | null, context: ErrorCodes, message: string) {
   if (!response) {
-    pushError(context, message);
+    pushError(context);
   } else if (isServerResponseError(response)) {
     console.log(`${message} with server response: ${response.error}`);
-    pushError(context, response.error);
+    pushError(context);
   } else {
     console.log(`${message} with server response: ${response}\n`);
-    pushError(context, message);
+    pushError(context);
   }
 }
