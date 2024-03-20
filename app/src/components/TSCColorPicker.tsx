b7d27ff0e81f02d53cc74848a1e30206f2f10bd6
rohnach29 <rohnach29@gmail.com>
2024-03-20 18:24:11 -0400

    Prettier format

--- app/src/components/TSCColorPicker.tsx
+++ app/src/components/TSCColorPicker.tsx
@@ -1,30 +1,33 @@
-import {observer} from "mobx-react-lite";
-import React, {useState} from "react";
-import { HSLColor, RGBColor, SketchPicker } from 'react-color';
-import {
-    Button,
-} from "@mui/material";
-import "./TSCColorPicker.css"
+import { observer } from "mobx-react-lite";
+import React, { useState } from "react";
+import { HSLColor, RGBColor, SketchPicker } from "react-color";
+import { Button } from "@mui/material";
+import "./TSCColorPicker.css";
 
 interface ColorResult {
-    hex: string;
-    rgb: RGBColor;
-    hsl: HSLColor;
+  hex: string;
+  rgb: RGBColor;
+  hsl: HSLColor;
 }
 
 const TSCColorPicker: React.FC = observer(() => {
-    const [showPicker, setShowPicker] = useState(false);
-    const [selectedColor, setSelectedColor] = useState('#000'); // State to keep track of selected color
-    
-    const handleColorChange = (color: ColorResult) => {
-      setSelectedColor(color.hex);
-    };
+  const [showPicker, setShowPicker] = useState(false);
+  const [selectedColor, setSelectedColor] = useState("#000"); // State to keep track of selected color
+
+  const handleColorChange = (color: ColorResult) => {
+    setSelectedColor(color.hex);
+  };
 
   return (
     <div>
-      <Button color="secondary" variant="contained" onClick={() => setShowPicker(!showPicker)} className="cp-button" style = {{backgroundColor: selectedColor}}></Button>
+      <Button
+        color="secondary"
+        variant="contained"
+        onClick={() => setShowPicker(!showPicker)}
+        className="cp-button"
+        style={{ backgroundColor: selectedColor }}></Button>
       {showPicker && (
-        <div style={{ position: 'absolute', zIndex: 2 }}>
+        <div style={{ position: "absolute", zIndex: 2 }}>
           {/* @ts-expect-error Server Component */}
           <SketchPicker color={selectedColor} onChange={handleColorChange} />
         </div>
@@ -32,6 +35,5 @@ const TSCColorPicker: React.FC = observer(() => {
     </div>
   );
 });
-  
 
-export default TSCColorPicker
+export default TSCColorPicker;
