9c8663c987dc865c8a251b414458a779bf1a5ba9
rohnach29 <rohnach29@gmail.com>
2024-03-20 18:16:45 -0400

    General Color Picker

---
+++ app/src/components/TSCColorPicker.tsx
@@ -0,0 +1,37 @@
+import {observer} from "mobx-react-lite";
+import React, {useState} from "react";
+import { HSLColor, RGBColor, SketchPicker } from 'react-color';
+import {
+    Button,
+} from "@mui/material";
+import "./TSCColorPicker.css"
+
+interface ColorResult {
+    hex: string;
+    rgb: RGBColor;
+    hsl: HSLColor;
+}
+
+const TSCColorPicker: React.FC = observer(() => {
+    const [showPicker, setShowPicker] = useState(false);
+    const [selectedColor, setSelectedColor] = useState('#000'); // State to keep track of selected color
+    
+    const handleColorChange = (color: ColorResult) => {
+      setSelectedColor(color.hex);
+    };
+
+  return (
+    <div>
+      <Button color="secondary" variant="contained" onClick={() => setShowPicker(!showPicker)} className="cp-button" style = {{backgroundColor: selectedColor}}></Button>
+      {showPicker && (
+        <div style={{ position: 'absolute', zIndex: 2 }}>
+          {/* @ts-expect-error Server Component */}
+          <SketchPicker color={selectedColor} onChange={handleColorChange} />
+        </div>
+      )}
+    </div>
+  );
+});
+  
+
+export default TSCColorPicker
