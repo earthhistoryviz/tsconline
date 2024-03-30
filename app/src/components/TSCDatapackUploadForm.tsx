380b9d9e146ff95735293dff36ae88fa0fda7897
paologum <pgumasin@purdue.edu>
2024-03-30 16:20:36 -0400

    incremental ui progress

--- app/src/components/TSCDatapackUploadForm.tsx
+++ app/src/components/TSCDatapackUploadForm.tsx
@@ -1,4 +1,4 @@
-import { TextField, Typography } from "@mui/material";
+import { IconButton, TextField, Typography } from "@mui/material";
 import { useContext, useState } from "react";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { InputFileUpload } from "./TSCFileUpload";
@@ -6,67 +6,81 @@ import { context } from "../state";
 import "./TSCDatapackUploadForm.css";
 import { TSCButton } from "./TSCButton";
 import { ErrorCodes } from "../util/error-codes";
+import CloseIcon from "@mui/icons-material/Close";
+import { CustomDivider } from "./TSCComponents";
 
-export const TSCDatapackUploadForm = ({}) => {
+type TSCDatapackUploadFormProps = {
+  close: () => void;
+};
+export const TSCDatapackUploadForm: React.FC<TSCDatapackUploadFormProps> = ({ close }) => {
   const { actions } = useContext(context);
   const [datapackName, setDatapackName] = useState("");
   const [datapackDescription, setDatapackDescription] = useState("");
   const [datapackFile, setDatapackFile] = useState<File | null>(null);
   return (
-    <div className="datapack-upload-form">
-      <Typography className="upload-datapack-header" variant="h4">
-        Upload a Datapack
-      </Typography>
-      <TextField
-        label="Datapack Name"
-        className="datapack-name-input"
-        value={datapackName}
-        onChange={(event) => setDatapackName(event.target.value)}
-      />
-      <TextField
-        multiline
-        rows={10}
-        label="Datapack Description"
-        placeholder="Enter a description for your datapack."
-        className="datapack-description-input"
-        inputProps={{ className: "datapack-description-input-text" }}
-        InputLabelProps={{ shrink: true }}
-        value={datapackDescription}
-        onChange={(event) => setDatapackDescription(event.target.value)}
-      />
-      <div className="file-upload">
-        <InputFileUpload
-          startIcon={<CloudUploadIcon />}
-          text="Upload Datapack"
-          onChange={(event) => {
-            const file = event.target.files![0];
-            if (!file) {
-              actions.pushError(ErrorCodes.NO_DATAPACK_FILE_FOUND);
-              return;
-            }
-            if (file.name.length > 50) {
-              actions.pushError(ErrorCodes.DATAPACK_FILE_NAME_TOO_LONG);
-              return;
-            }
-            setDatapackFile(file);
-          }}
-        />
-        <Typography className="file-upload-text" variant="body2">
-          {datapackFile ? datapackFile.name : "No file selected"}
+    <>
+      <div className="close-upload-form">
+        <IconButton className="icon" onClick={close} size="large">
+          <CloseIcon className="close-icon" />
+        </IconButton>
+      </div>
+      <div className="datapack-upload-form">
+        <Typography className="upload-datapack-header" variant="h4">
+          Upload a Datapack
         </Typography>
+        <CustomDivider />
+        <div className="file-upload">
+          <InputFileUpload
+            startIcon={<CloudUploadIcon />}
+            text="Upload Datapack"
+            onChange={(event) => {
+              const file = event.target.files![0];
+              if (!file) {
+                return;
+              }
+              if (file.name.length > 50) {
+                actions.pushError(ErrorCodes.DATAPACK_FILE_NAME_TOO_LONG);
+                return;
+              }
+              actions.removeError(ErrorCodes.NO_DATAPACK_FILE_FOUND);
+              actions.removeError(ErrorCodes.DATAPACK_FILE_NAME_TOO_LONG);
+              setDatapackFile(file);
+            }}
+          />
+          <Typography className="file-upload-text" variant="body2">
+            {datapackFile ? datapackFile.name : "No file selected"}
+          </Typography>
+        </div>
+        <TextField
+          label="Datapack Name"
+          className="datapack-name-input"
+          value={datapackName}
+          onChange={(event) => setDatapackName(event.target.value)}
+        />
+        <TextField
+          multiline
+          rows={10}
+          label="Datapack Description"
+          placeholder="Enter a description for your datapack."
+          className="datapack-description-input"
+          inputProps={{ className: "datapack-description-input-text" }}
+          InputLabelProps={{ shrink: true }}
+          value={datapackDescription}
+          onChange={(event) => setDatapackDescription(event.target.value)}
+        />
+        <div className="file-upload-button">
+          <TSCButton
+            onClick={() => {
+              if (!datapackFile) {
+                actions.pushError(ErrorCodes.NO_DATAPACK_FILE_FOUND);
+                return;
+              }
+              actions.uploadDatapack(datapackFile!, "username");
+            }}>
+            Upload
+          </TSCButton>
+        </div>
       </div>
-      <div className="file-upload-button">
-        <TSCButton
-          onClick={() => {
-            if (!datapackFile) {
-              actions.pushError(ErrorCodes.NO_DATAPACK_FILE_FOUND);
-              return;
-            }
-            actions.uploadDatapack(datapackFile!, "username");
-          }}>
-          Upload
-        </TSCButton>
-      </div>
-    </div>
+    </>
   );
 };
