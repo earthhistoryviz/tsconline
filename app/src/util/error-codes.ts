81d251d7d83f379f62bd0eabde88f44628c93bf2
paologum <pgumasin@purdue.edu>
2024-03-31 13:50:48 -0400

    prettier

--- app/src/util/error-codes.ts
+++ app/src/util/error-codes.ts
@@ -17,7 +17,7 @@ export enum ErrorCodes {
   DATAPACK_FILE_NAME_TOO_LONG = "DATAPACK_FILE_NAME_TOO_LONG",
   UNRECOGNIZED_DATAPACK_EXTENSION = "UNRECOGNIZED_DATAPACK_EXTENSION",
   UNRECOGNIZED_DATAPACK_FILE = "UNRECOGNIZED_DATAPACK_FILE",
-  UNFINISHED_DATAPACK_UPLOAD_FORM = "UNFINISHED_DATAPACK_UPLOAD_FORM",
+  UNFINISHED_DATAPACK_UPLOAD_FORM = "UNFINISHED_DATAPACK_UPLOAD_FORM"
 }
 
 export const ErrorMessages = {
@@ -37,7 +37,9 @@ export const ErrorMessages = {
   [ErrorCodes.INVALID_USER_DATAPACKS]: "Invalid user datapacks received from server. Please try again later.",
   [ErrorCodes.NO_DATAPACK_FILE_FOUND]: "No datapack file found. Please upload a datapack file first.",
   [ErrorCodes.DATAPACK_FILE_NAME_TOO_LONG]: "Datapack file name is too long. Please shorten the file name.",
-  [ErrorCodes.UNRECOGNIZED_DATAPACK_EXTENSION]: "Unrecognized datapack extension. File must be of type .dpk, .mdpk, .map, .txt. Please upload a valid datapack file.",
+  [ErrorCodes.UNRECOGNIZED_DATAPACK_EXTENSION]:
+    "Unrecognized datapack extension. File must be of type .dpk, .mdpk, .map, .txt. Please upload a valid datapack file.",
   [ErrorCodes.UNRECOGNIZED_DATAPACK_FILE]: "Unrecognized datapack file. Please upload a valid datapack file.",
-  [ErrorCodes.UNFINISHED_DATAPACK_UPLOAD_FORM]: "Please finish the datapack upload form before attempting to upload the file.",
+  [ErrorCodes.UNFINISHED_DATAPACK_UPLOAD_FORM]:
+    "Please finish the datapack upload form before attempting to upload the file."
 };
