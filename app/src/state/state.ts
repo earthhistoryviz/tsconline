b1a6a7b4053b0da782337f39fed2abb67164931d
rohnach29 <rohnach29@gmail.com>
2024-04-01 15:49:00 -0400

    Rebase, prettier format

--- app/src/state/state.ts
+++ app/src/state/state.ts
@@ -18,9 +18,16 @@ import { ErrorCodes } from "../util/error-codes";
 import { settings } from "../constants";
 
 const defaultColors: string[] = [
-  "#D32F2F", "#C2185B", "#7B1FA2", "#512DA8",
-  "#303F9F", "#1976D2", "#0288D1", "#0097A7",
-  "#00796B", "#388E3C"
+  "#D32F2F",
+  "#C2185B",
+  "#7B1FA2",
+  "#512DA8",
+  "#303F9F",
+  "#1976D2",
+  "#0288D1",
+  "#0097A7",
+  "#00796B",
+  "#388E3C"
 ];
 
 export type State = {
