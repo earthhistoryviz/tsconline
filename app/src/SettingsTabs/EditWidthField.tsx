65a5b00408a67b1feb95e39b0a6f7ee6c49f447c
Bostre, Leyton Drew <lbostre@purdue.edu>
2024-04-01 17:02:06 -0400

    changed width input to NumericFormat

--- app/src/SettingsTabs/EditWidthField.tsx
+++ app/src/SettingsTabs/EditWidthField.tsx
@@ -1,5 +1,5 @@
 import { observer } from "mobx-react-lite";
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import { context } from "../state";
 import { TextFieldProps, Typography, TextField } from "@mui/material";
 import "./EditWidthField.css";
