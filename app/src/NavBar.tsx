2dcbaecce60074df2cd8c0966f4b8f625f80b555
tobyonyekwere1 <tobyonyekwere1@gmail.com>
2024-04-03 17:04:35 -0500

    finished hover

--- app/src/NavBar.tsx
+++ app/src/NavBar.tsx
@@ -1,20 +1,35 @@
-import { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { observer } from "mobx-react-lite";
+import { useNavigate } from "react-router-dom"; // Import useNavigate from React Router
 import AppBar from "@mui/material/AppBar";
 import { Link } from "react-router-dom";
 import Toolbar from "@mui/material/Toolbar";
 import { useTheme } from "@mui/material/styles";
 import TSCreatorLogo from "./assets/TSCreatorLogo.png";
 import HomeIcon from "@mui/icons-material/Home";
-import { IconButton, Tab } from "@mui/material";
+import { IconButton } from "@mui/material";
 import { context } from "./state";
-import { TSCTabs } from "./components";
+import { TSCTabs, Tab } from "./components";
+import { ControlledMenu, MenuItem, useMenuState, useHover } from "@szhsin/react-menu";
+import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/transitions/slide.css";
 
 import "./NavBar.css";
 
 export const NavBar = observer(function Navbar() {
+  const navigate = useNavigate(); // Hook from React Router for navigation
   const theme = useTheme();
   const { state, actions } = useContext(context);
+  const menuRef = useRef(null);
+  const [menuState, toggleMenu] = useMenuState({ transition: true });
+  const { hoverProps } = useHover(menuState.state, toggleMenu);
+
+  // Function to handle menu item click
+  const handleMenuItemClick = (selectedTab) => {
+    actions.setSettingsTabsSelected(selectedTab); // Set the selected settings tab
+    navigate('/settings'); // Navigate to the settings page
+  };
+
   return (
     <AppBar position="fixed" sx={{ background: theme.palette.navbar.main, display: "flex" }}>
       <Toolbar>
@@ -28,40 +43,43 @@ export const NavBar = observer(function Navbar() {
                 opacity: 1
               }
             }}
-            value={0}
             onClick={() => {
               actions.setTab(0);
               actions.setUseCache(true);
-            }}>
+            }}
+          >
             <HomeIcon />
           </IconButton>
         </Link>
-        {
-          <TSCTabs
-            value={state.tab !== 0 ? state.tab : false}
-            onChange={(_e, value) => {
-              actions.setTab(value);
-            }}
-            //override the TSCTabs since it has the dark navbar
-            sx={{
-              "& .MuiTab-root": {
-                color: theme.palette.primary.main,
-                "&:hover": {
-                  color: theme.palette.selection.light
-                }
-              },
-              "& .Mui-selected": {
-                color: theme.palette.selection.main
+        <TSCTabs
+          value={state.tab !== 0 ? state.tab : false}
+          onChange={(_e, value) => actions.setTab(value)}
+          sx={{
+            "& .MuiTab-root": {
+              color: theme.palette.primary.main,
+              "&:hover": {
+                color: theme.palette.selection.light
               }
-            }}>
-            <Tab value={1} label="Chart" to="/chart" component={Link} />
-            <Tab value={2} label="Settings" to="/settings" component={Link} />
-            <Tab value={3} label="Help" to="/help" component={Link} />
-            <Tab value={4} label="About" to="/about" component={Link} />
-          </TSCTabs>
-        }
+            },
+            "& .Mui-selected": {
+              color: theme.palette.selection.main
+            }
+          }}
+        >
+          <Tab label="Chart" value={1} component={Link} to="/chart" />
+          <Tab label="Settings" value={2} component={Link} to="/settings" ref={menuRef} {...hoverProps} />
+          <Tab label="Datapack" value={3} component={Link} to="/datapack" />
+          <Tab label="Help" value={4} component={Link} to="/help" />
+          <Tab label="About" value={5} component={Link} to="/about" />
+        </TSCTabs>
         <div style={{ flexGrow: 1 }} />
-        <img src={TSCreatorLogo} width="4%" height="4%" />
+        <img src={TSCreatorLogo} alt="TSCreator Logo" width="4%" height="4%" />
+        <ControlledMenu {...hoverProps} anchorRef={menuRef} onClose={() => toggleMenu(false)}>
+          <MenuItem onClick={() => handleMenuItemClick('time')}>Time</MenuItem>
+          <MenuItem onClick={() => handleMenuItemClick('font')}>Font</MenuItem>
+          <MenuItem onClick={() => handleMenuItemClick('column')}>Columns</MenuItem>
+          <MenuItem onClick={() => handleMenuItemClick('mappoints')}>Map Points</MenuItem>
+        </ControlledMenu>
       </Toolbar>
     </AppBar>
   );
