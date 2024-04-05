719907464eb142d72e0e813f4e050d469ac1d461
asivath <asivatha@purdue.edu>
2024-04-05 10:39:56 -0400

    added login screen

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { context, initialContext } from "./state";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <context.Provider value={initialContext}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </context.Provider>
-  </React.StrictMode>
+  // Aditya's client ID needed for Google OAuth, should be replaced with Aaron's client ID
+  <GoogleOAuthProvider clientId="1080920977375-djjnsjd5dpj4eopgfadkt9keukc57m8b.apps.googleusercontent.com">
+    <React.StrictMode>
+      <context.Provider value={initialContext}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </context.Provider>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
