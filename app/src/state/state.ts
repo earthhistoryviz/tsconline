f9652f539e9a4c115ce13ff151ca920a7c23daff
rohnach29 <rohnach29@gmail.com>
2024-04-10 17:14:54 -0400

    Connect to font options, other PR changes

--- app/src/state/state.ts
+++ app/src/state/state.ts
@@ -17,19 +17,6 @@ import type {
 import { ErrorCodes } from "../util/error-codes";
 import { settings } from "../constants";
 
-const defaultColors: string[] = [
-  "#D32F2F",
-  "#C2185B",
-  "#7B1FA2",
-  "#512DA8",
-  "#303F9F",
-  "#1976D2",
-  "#0288D1",
-  "#0097A7",
-  "#00796B",
-  "#388E3C"
-];
-
 export type State = {
   isLoggedIn: boolean;
   chartLoading: boolean;
