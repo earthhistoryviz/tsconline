1e0688083e8838489ae23e4fa1480884df03e8e0
Bostre, Leyton Drew <lbostre@purdue.edu>
2024-04-12 11:44:21 -0400

    check-quality

--- app/src/SettingsTabs/InfoBox.tsx
+++ app/src/SettingsTabs/InfoBox.tsx
@@ -1,27 +1,24 @@
 import { observer } from "mobx-react-lite";
-import React, { useContext} from "react";
+import { useContext } from "react";
 import { context } from "../state";
-import {
-    Box,
-    Typography,
-} from "@mui/material";
-import "./InfoBox.css"
-import {StyledScrollbar} from "../components";
+import { Box, Typography } from "@mui/material";
+import "./InfoBox.css";
+import { StyledScrollbar } from "../components";
 
 export const InfoBox = observer(() => {
-    const { state } = useContext(context);
-    const info =
-        state.settingsTabs.columnSelected === null
-            ? ""
-            : state.settingsTabs.columnHashMap.get(state.settingsTabs.columnSelected)!.popup;
-    return (
-        <div className="container">
-            <Typography style={{fontWeight: "bold"}}>Information and References</Typography>
-            <Box className="info-box">
-                <StyledScrollbar className="scroll-bar">
-                    <div dangerouslySetInnerHTML={{__html: info.substring(1, info.length - 1).replace('""', '"')}}/>
-                </StyledScrollbar>
-            </Box>
-        </div>
-    );
+  const { state } = useContext(context);
+  const info =
+    state.settingsTabs.columnSelected === null
+      ? ""
+      : state.settingsTabs.columnHashMap.get(state.settingsTabs.columnSelected)!.popup;
+  return (
+    <div className="container">
+      <Typography style={{ fontWeight: "bold" }}>Information and References</Typography>
+      <Box className="info-box">
+        <StyledScrollbar className="scroll-bar">
+          <div dangerouslySetInnerHTML={{ __html: info.substring(1, info.length - 1).replace('""', '"') }} />
+        </StyledScrollbar>
+      </Box>
+    </div>
+  );
 });
