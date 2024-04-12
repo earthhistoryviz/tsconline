7df3d36d145a324faf37998f754b4bbbea11278a
Bostre, Leyton Drew <lbostre@purdue.edu>
2024-04-12 09:08:25 -0400

    More PR Changes

--- app/src/SettingsTabs/EditWidthField.tsx
+++ app/src/SettingsTabs/EditWidthField.tsx
@@ -1,11 +1,10 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext, useState } from "react";
 import { context } from "../state";
-import { Button, TextFieldProps, Typography, TextField } from "@mui/material";
+import { TextFieldProps, Typography, TextField } from "@mui/material";
 import "./EditWidthField.css";
 import { NumericFormat } from "react-number-format";
-import { pushError } from "../state/actions";
-import { ErrorCodes } from "../util/error-codes";
+import { ColumnInfo } from "@tsconline/shared";
 
 const WidthTextField = ({ ...props }: TextFieldProps) => (
   <TextField {...props} hiddenLabel variant="filled" size="small" />
@@ -14,36 +13,31 @@ const WidthTextField = ({ ...props }: TextFieldProps) => (
 export const EditWidthField: React.FC<{
   width: number;
 }> = observer(({ width }) => {
-  const { actions } = useContext(context);
-  const [inputWidth, setInputWidth] = useState<number>(width ?? 0);
+  const { state, actions } = useContext(context);
+  const [inputWidth, setInputWidth] = useState<number>(width ?? 20);
+  let columnObject: ColumnInfo | undefined;
+  if (state.settingsTabs.columnSelected != null) {
+    columnObject = state.settingsTabs.columnHashMap.get(state.settingsTabs.columnSelected);
+  }
   return (
     <div>
       <Typography id="edit-width-text">Edit Width</Typography>
-      <div id="edit-width-container">
-        <NumericFormat
-          value={inputWidth}
-          customInput={WidthTextField}
-          onValueChange={(values) => {
-            const floatValue = values.floatValue;
-            if (!floatValue) {
-              return;
-            }
+      <NumericFormat
+        value={inputWidth || ""}
+        customInput={WidthTextField}
+        onValueChange={(values) => {
+          const floatValue = values.floatValue;
+          if (floatValue === undefined && columnObject) {
+            actions.updateWidth(columnObject, 20);
+            return;
+          }
+          if (floatValue !== undefined) {
             setInputWidth(floatValue);
-          }}
-          style={{ height: "20px" }}
-        />
-        <div>
-          <Button
-            color="secondary"
-            variant="contained"
-            onClick={() => {
-              if (isNaN(inputWidth)) pushError(ErrorCodes.INVALID_WIDTH_TYPE);
-              actions.updateWidth(inputWidth);
-            }}>
-            Confirm
-          </Button>
-        </div>
-      </div>
+            if (columnObject) actions.updateWidth(columnObject, floatValue);
+          }
+        }}
+        style={{ height: "20px" }}
+      />
     </div>
   );
 });
