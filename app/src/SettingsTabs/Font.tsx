126b3cf167b469635c2fc7ea61272d5e838ff439
paologum <pgumasin@purdue.edu>
2024-04-23 22:20:42 -0400

    font menu for chart root

--- app/src/SettingsTabs/Font.tsx
+++ app/src/SettingsTabs/Font.tsx
@@ -1,23 +1,39 @@
 import { observer } from "mobx-react-lite";
 import { Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import { context } from "../state";
+import { useContext } from "react";
+import { LeafColumnFontMenu } from "./FontMenu";
+import "./Font.css"
 export const Font = observer(function Font() {
   const theme = useTheme();
+  const { state } = useContext(context)
+  const rootColumn = state.settingsTabs.columns
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        minHeight: "100vh"
-      }}>
+    <>
+    {rootColumn ? 
+    (
+    <div className="root-font-options">
+      <div className="root-font-description">
+        <Typography> These are the default systemwide fonts.</Typography>
+        <Typography> Changing one here will affect the entire chart.</Typography>
+        <Typography> NOTE: These fonts can be changed on a column by column basis by using the Font button for each column</Typography>
+      </div>
+      <LeafColumnFontMenu column={rootColumn} />
+    </div>
+    )
+    :
+    (    
+    <div className="chart-root-not-available">
       <Typography
         sx={{
           fontSize: theme.typography.pxToRem(18)
         }}>
-        Font Settings are currently not supported
+          No Datapack is Selected
       </Typography>
     </div>
+)
+    }
+    </>
   );
 });
