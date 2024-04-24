f065d226d89a472579ab09b9b7ba3ffb9fae8ed4
Sejal Kumar <kumarsb@purdue.edu>
2024-04-23 20:05:32 -0400

    removed generate button

--- app/src/SettingsTabs/Column.tsx
+++ app/src/SettingsTabs/Column.tsx
@@ -212,13 +212,13 @@ export const Column = observer(function Column() {
           </Typography>
         )}
       </Box>
-      <TSCButton
+      {/* <TSCButton
         id="column-generate-button-top"
         onClick={() => {
           actions.fetchChartFromServer(navigate);
         }}>
         Generate
-      </TSCButton>
+      </TSCButton> */}
       <div className="column-accordion-and-menu">
         <Box
           className={`hide-scrollbar column-accordion-wrapper ${searchTerm ? 'filtered-border' : ''}`}
