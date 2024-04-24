4dd52ed08df7db797946f74507c983041b0579c5
paologum <pgumasin@purdue.edu>
2024-04-23 22:23:20 -0400

    font menu formatting

--- app/src/SettingsTabs/Font.tsx
+++ app/src/SettingsTabs/Font.tsx
@@ -4,36 +4,35 @@ import { useTheme } from "@mui/material/styles";
 import { context } from "../state";
 import { useContext } from "react";
 import { LeafColumnFontMenu } from "./FontMenu";
-import "./Font.css"
+import "./Font.css";
 export const Font = observer(function Font() {
   const theme = useTheme();
-  const { state } = useContext(context)
-  const rootColumn = state.settingsTabs.columns
+  const { state } = useContext(context);
+  const rootColumn = state.settingsTabs.columns;
   return (
     <>
-    {rootColumn ? 
-    (
-    <div className="root-font-options">
-      <div className="root-font-description">
-        <Typography> These are the default systemwide fonts.</Typography>
-        <Typography> Changing one here will affect the entire chart.</Typography>
-        <Typography> NOTE: These fonts can be changed on a column by column basis by using the Font button for each column</Typography>
-      </div>
-      <LeafColumnFontMenu column={rootColumn} />
-    </div>
-    )
-    :
-    (    
-    <div className="chart-root-not-available">
-      <Typography
-        sx={{
-          fontSize: theme.typography.pxToRem(18)
-        }}>
-          No Datapack is Selected
-      </Typography>
-    </div>
-)
-    }
+      {rootColumn ? (
+        <div className="root-font-options">
+          <div className="root-font-description">
+            <Typography> These are the default systemwide fonts.</Typography>
+            <Typography> Changing one here will affect the entire chart.</Typography>
+            <Typography>
+              {" "}
+              NOTE: These fonts can be changed on a column by column basis by using the Font button for each column
+            </Typography>
+          </div>
+          <LeafColumnFontMenu column={rootColumn} />
+        </div>
+      ) : (
+        <div className="chart-root-not-available">
+          <Typography
+            sx={{
+              fontSize: theme.typography.pxToRem(18)
+            }}>
+            No Datapack is Selected
+          </Typography>
+        </div>
+      )}
     </>
   );
 });
