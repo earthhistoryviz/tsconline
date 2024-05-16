533e40c4faeab26125cb133fb17f0f97b4626d66
jay-jhk-lee <jaylee.62893@gmail.com>
2024-05-16 14:18:58 -0400

    added load button

--- app/src/Settings.tsx
+++ app/src/Settings.tsx
@@ -7,7 +7,10 @@ import { Font } from "./settings_tabs/Font";
 import { MapPoints } from "./settings_tabs/map_points/MapPoints";
 import { Datapacks } from "./settings_tabs/Datapack";
 import { useTheme } from "@mui/material/styles";
-import { TSCTabs, TSCTab } from "./components";
+import { TSCTabs, TSCTab, InputFileUpload } from "./components";
+import DownloadIcon from "@mui/icons-material/Download";
+import { applySettings, pushSnackbar } from "./state/actions";
+import { xmlToJson } from "./state/parse-settings";
 
 export const Settings = observer(function Settings() {
   const { state, actions } = useContext(context);
@@ -34,8 +37,28 @@ export const Settings = observer(function Settings() {
     }
   }
 
+  function loadSettings(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files) {
+      pushSnackbar("failed to load settings: no files uploaded", "warning");
+      return;
+    }
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    if (reader.result && typeof reader.result === "string") {
+      try {
+        applySettings(xmlToJson(reader.result));
+      } catch (e) {
+        pushSnackbar("Failed to load settings: file content is in wrong format", "warning");
+      }
+    } else {
+      pushSnackbar("Falied to load settings: file content is not a string", "warning");
+    }
+  }
+
   return (
     <div style={{ background: theme.palette.settings.light, overflow: "auto" }}>
+      <InputFileUpload startIcon={<DownloadIcon />} text={"load"} onChange={(e) => loadSettings(e)} />
       <TSCTabs value={selectedTabIndex} onChange={handleChange} centered>
         <TSCTab label="Time" onClick={() => actions.setSettingsTabsSelected("time")} />
         <TSCTab label="Column" onClick={() => actions.setSettingsTabsSelected("column")} />
