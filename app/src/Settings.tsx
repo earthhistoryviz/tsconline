fbe25b5180499436fb202f3d64b87114d87ab29e
jay-jhk-lee <jaylee.62893@gmail.com>
2024-05-17 14:56:10 -0400

    added filename field for save settings

--- app/src/Settings.tsx
+++ app/src/Settings.tsx
@@ -7,15 +7,11 @@ import { Font } from "./settings_tabs/Font";
 import { MapPoints } from "./settings_tabs/map_points/MapPoints";
 import { Datapacks } from "./settings_tabs/Datapack";
 import { useTheme } from "@mui/material/styles";
-import { TSCTabs, TSCTab, InputFileUpload, TSCButton } from "./components";
-import DownloadIcon from "@mui/icons-material/Download";
+import { TSCTabs, TSCTab, InputFileUpload } from "./components";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
-import { jsonToXml, xmlToJson } from "./state/parse-settings";
-import { cloneDeep } from "lodash";
-import { ColumnInfo } from "@tsconline/shared";
-import { ChartSettings } from "./types";
-import FileSaver from "file-saver";
+import { xmlToJson } from "./state/parse-settings";
 import { Typography } from "@mui/material";
+import SaveSettings from "./SettingsTabs/SaveSettings";
 
 export const Settings = observer(function Settings() {
   const { state, actions } = useContext(context);
@@ -48,27 +44,15 @@ export const Settings = observer(function Settings() {
     }
     const file = e.target.files[0];
     const data = await file.text();
+    //console.log(data);
     try {
       actions.applySettings(xmlToJson(data));
     } catch (e) {
+      console.log(e);
       actions.pushSnackbar("Failed to load settings: file content is in wrong format", "info");
-    }
-    actions.pushSnackbar("successfully loaded settings!", "success");
-  }
-
-  function saveSettings() {
-    if (!state.settingsTabs.columns) {
-      actions.pushSnackbar("Column Root is not set, try again after selecting a datapack", "info");
       return;
     }
-    if (state.settingsTabs.columns.children.length === 0) {
-      actions.pushSnackbar("No columns are set, proceeding...", "info");
-    }
-    const columnCopy: ColumnInfo = cloneDeep(state.settingsTabs.columns!);
-    const settingsCopy: ChartSettings = cloneDeep(state.settings);
-    const blob = new Blob([jsonToXml(columnCopy, settingsCopy)], { type: "text/plain;charset=utf-8" });
-    FileSaver.saveAs(blob, "settings.tsc");
-    actions.pushSnackbar("successfully saved settings!", "success");
+    actions.pushSnackbar("successfully loaded settings!", "success");
   }
 
   const SettingsHeader = () => {
@@ -87,15 +71,13 @@ export const Settings = observer(function Settings() {
         </Typography>
         <div style={{ flex: "1", textAlign: "right", marginTop: "5vh" }}>
           <InputFileUpload startIcon={<FileUploadIcon />} text={"load"} onChange={(e) => loadSettings(e)} />
-          <TSCButton startIcon={<DownloadIcon />} onClick={saveSettings}>
-            save
-          </TSCButton>
+          <SaveSettings />
         </div>
       </div>
     );
   };
   return (
-    <div style={{ background: theme.palette.settings.light, overflow: "auto" }}>
+    <div style={{ background: theme.palette.settings.light, overflow: "auto", wordSpacing: "1" }}>
       <SettingsHeader />
       <TSCTabs value={selectedTabIndex} onChange={handleChange} centered>
         <TSCTab label="Time" onClick={() => actions.setSettingsTabsSelected("time")} />
---
+++ app/src/settings_tabs/SaveSettings.tsx
@@ -0,0 +1,85 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import FileSaver from "file-saver";
+import { context } from "../state";
+import { ColumnInfo } from "@tsconline/shared";
+import { cloneDeep } from "lodash";
+import { ChartSettings } from "../types";
+import { jsonToXml } from "../state/parse-settings";
+import { IconButton } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
+export default function FormDialog() {
+  const { state, actions } = React.useContext(context);
+  function saveSettings(filename: string) {
+    if (!state.settingsTabs.columns) {
+      actions.pushSnackbar("Column Root is not set, try again after selecting a datapack", "info");
+      return;
+    }
+    if (state.settingsTabs.columns.children.length === 0) {
+      actions.pushSnackbar("No columns are set, proceeding...", "info");
+    }
+    const columnCopy: ColumnInfo = cloneDeep(state.settingsTabs.columns!);
+    const settingsCopy: ChartSettings = cloneDeep(state.settings);
+    const blob = new Blob([jsonToXml(columnCopy, settingsCopy)], { type: "text/plain;charset=utf-8" });
+    FileSaver.saveAs(blob, filename);
+    actions.pushSnackbar("successfully saved settings!", "success");
+  }
+  const [open, setOpen] = React.useState(false);
+  const [filename, setFilename] = React.useState("settings.tsc");
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleChange = (e: { target: { value: React.SetStateAction<string> } }) => {
+    setFilename(e.target.value);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <IconButton onClick={handleClickOpen}>
+        <DownloadIcon />
+      </IconButton>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: "form",
+          onSubmit: () => {
+            //TODO: check filename format
+            saveSettings(filename);
+            handleClose();
+          }
+        }}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Please enter the filename below.</DialogContentText>
+          <TextField
+            defaultValue={filename}
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="filename"
+            label="filename"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={handleChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Save</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
