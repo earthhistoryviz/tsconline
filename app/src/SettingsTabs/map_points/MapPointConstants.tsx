8a727df2ba52419bcfc42026f8f8d04c581275a7
Paolo Gumasing <paologum@gmail.com>
2024-05-21 21:12:21 -0400

    changed settings_tabs and reformatted

--- app/src/SettingsTabs/map_points/MapPointConstants.tsx
+++
@@ -1,6 +0,0 @@
-// height of header bar without the controls
-export const NormHeaderHeight = "10vh";
-// this is to account for the added controls
-export const FaciesHeaderHeight = "20vh";
-// the header of the legend (need for scrolling to function properly on facies buttons)
-export const LegendHeaderHeight = "8vh";
