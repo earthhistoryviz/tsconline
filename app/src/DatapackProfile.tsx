621175e463eaba55fcb581f303fa196c41b2b4c9
Paolo Gumasing <paologum@gmail.com>
2024-06-13 02:42:03 -0400

    fixed root color and more adjustments to datapack profile

--- app/src/DatapackProfile.tsx
+++ app/src/DatapackProfile.tsx
@@ -5,10 +5,11 @@ import { useContext, useState } from "react";
 import { context } from "./state";
 import { devSafeUrl } from "./util";
 import { IconButton, Typography } from "@mui/material";
-import { CustomDivider } from "./components";
+import { CustomDivider, TagButton } from "./components";
 import { CustomTabs } from "./components/TSCCustomTabs";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
+const tags: string[] = ["Large", "Small", "Medium", "Huge", "Tiny", "Normal", "Abnormal"];
 export const DatapackProfile = observer(() => {
   const { state } = useContext(context);
   const { id } = useParams();
@@ -19,23 +20,25 @@ export const DatapackProfile = observer(() => {
   const datapack = state.datapackIndex[id];
   if (!datapack) return;
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <IconButton className={styles.back} onClick={() => navigate("/settings")}>
-          <ArrowBackIcon />
-        </IconButton>
-        <img className={styles.di} src={datapack.image || defaultImageUrl} />
-        <Typography className={styles.ht}>{id}</Typography>
+    <div className={styles.adjcontainer}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <IconButton className={styles.back} onClick={() => navigate("/settings")}>
+            <ArrowBackIcon className={styles.icon} />
+          </IconButton>
+          <Typography className={styles.ht}>{id}</Typography>
+          <img className={styles.di} src={datapack.image || defaultImageUrl} />
+        </div>
+        <CustomTabs
+          className={styles.tabs}
+          centered
+          value={tabIndex}
+          onChange={(val) => setTabIndex(val)}
+          tabs={["About", "Discussion (0)", "Data"]}
+        />
+        <CustomDivider className={styles.divider} />
+        <About />
       </div>
-      <CustomTabs
-        className={styles.tabs}
-        centered
-        value={tabIndex}
-        onChange={(val) => setTabIndex(val)}
-        tabs={["About", "Discussion", "Data"]}
-      />
-      <CustomDivider className={styles.divider} />
-      <About />
     </div>
   );
 });
@@ -51,21 +54,46 @@ const About: React.FC = () => {
           make a type specimen book. It has survived not only five centuries, but also the leap into electronic
           typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
           sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem Ipsum
+          PageMaker including versions of Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+          printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+          centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised
+          in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
+          desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum Lorem Ipsum is simply dummy
+          text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum
         </Typography>
       </div>
       <div className={styles.additional}>
         <div className={styles.ai}>
-          <Typography className={styles.aih}>File Size:</Typography>
-          <Typography>10 Mb</Typography>
+          <Typography className={styles.aih}>Author</Typography>
+          <Typography>Duval et al.</Typography>
         </div>
         <div className={styles.ai}>
-          <Typography className={styles.aih}>Columns:</Typography>
+          <Typography className={styles.aih}>Created</Typography>
+          <Typography>12/12/2004</Typography>
+        </div>
+        <div className={styles.ai}>
+          <Typography className={styles.aih}>Columns</Typography>
           <Typography>50</Typography>
         </div>
         <div className={styles.ai}>
-          <Typography className={styles.aih}>Created:</Typography>
-          <Typography>12/12/2004</Typography>
+          <Typography className={styles.aih}>File Size</Typography>
+          <Typography>10 Mb</Typography>
+        </div>
+        <div className={styles.ai}>
+          <Typography className={styles.aih}>Tags</Typography>
+          <div className={styles.tags}>
+            {tags.map((tag) => (
+              <TagButton key={tag}>
+                <Typography>{tag}</Typography>
+              </TagButton>
+            ))}
+          </div>
         </div>
       </div>
     </div>
