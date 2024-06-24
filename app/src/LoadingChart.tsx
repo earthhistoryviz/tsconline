11bde0d61a2ce0f181a58654edeed8d0d94edee9
paologum <pgumasin@purdue.edu>
2024-06-24 14:04:27 -0400

    added changes to chart and loading screen styles

--- app/src/LoadingChart.tsx
+++ app/src/LoadingChart.tsx
@@ -1,22 +1,19 @@
-import { useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import { Lottie } from "./components";
 import loader from "./assets/icons/loading.json";
 
 const LoadingChart = () => {
   const theme = useTheme();
   return (
-    <div
-      className="loading-container"
-      style={{
-        fontFamily: theme.typography.fontFamily,
-        background: theme.palette.settings.light
-      }}>
+    <Box
+    bgcolor="background.main"
+      className="loading-container">
       <Lottie animationData={loader} autoplay loop width={200} height={200} speed={0.7} />
       <div>
-        <h1 className="loading"> Loading Chart...</h1>
-        <p className="loading-sub"> (this could take more than a minute)</p>
+        <Typography variant="h1" className="loading"> Loading Chart...</Typography>
+        <Typography className="loading-sub"> (this could take more than a minute)</Typography>
       </div>
-    </div>
+    </Box>
   );
 };
 export default LoadingChart;
