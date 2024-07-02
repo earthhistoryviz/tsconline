478a8644af848bf504b5bf80b70aaf30209af9c5
paologum <pgumasin@purdue.edu>
2024-07-02 03:41:58 -0400

    added delete user for admin

--- server/src/admin-auth.ts
+++ server/src/admin-auth.ts
@@ -1,57 +1,75 @@
-import { FastifyInstance, FastifyPluginAsync, FastifyReply, FastifyRequest, RegisterOptions, RouteOptions } from "fastify";
-import { findUser } from "./database";
-import { createAdminOriginUser, getUsers } from "./admin-routes";
-import { checkRecaptchaToken } from "./verify";
-import { googleRecaptchaBotThreshold } from "./login-routes";
+import { FastifyInstance, FastifyReply, FastifyRequest, RegisterOptions } from "fastify";
+import { findUser } from "./database.js";
+import { adminCreateUser, adminDeleteUser, getUsers } from "./admin-routes.js";
+import { checkRecaptchaToken } from "./verify.js";
+import { googleRecaptchaBotThreshold } from "./login-routes.js";
 
 async function verifyAdmin(request: FastifyRequest, reply: FastifyReply) {
-    const uuid = request.session.get("uuid");
-    if (!uuid) {
-        reply.status(401).send({ message: "Unauthorized access" });
-        return;
-    }
-    const user = await findUser({ uuid });
-    if (!user || user.length !== 1) {
-        reply.status(401).send({ message: "Unauthorized access" });
-        return;
-    }
-    const { isAdmin } = user[0]!;
-    if (!isAdmin) {
-        reply.status(401).send({ message: "Unauthorized access" });
-        return;
-    }
+  const uuid = request.session.get("uuid");
+  if (!uuid) {
+    reply.status(401).send({ message: "Unauthorized access" });
+    return;
+  }
+  const user = await findUser({ uuid });
+  if (!user || user.length !== 1) {
+    reply.status(401).send({ message: "Unauthorized access" });
+    return;
+  }
+  const { isAdmin } = user[0]!;
+  if (!isAdmin) {
+    reply.status(401).send({ message: "Unauthorized access" });
+    return;
+  }
 }
 
 async function verifyRecaptcha(request: FastifyRequest, reply: FastifyReply) {
-    const recaptcha = request.headers["recaptcha-token"];
-    if (!recaptcha || typeof recaptcha !== "string") {
-        reply.status(400).send({ message: "Missing recaptcha token" });
-        return;
-    }
-    const score = await checkRecaptchaToken(recaptcha);
-    if (score < googleRecaptchaBotThreshold) {
-        reply.status(422).send({ message: "Recaptcha failed" });
-        return;
-    }
+  const recaptcha = request.headers["recaptcha-token"];
+  if (!recaptcha || typeof recaptcha !== "string") {
+    reply.status(400).send({ message: "Missing recaptcha token" });
+    return;
+  }
+  const score = await checkRecaptchaToken(recaptcha);
+  if (score < googleRecaptchaBotThreshold) {
+    reply.status(422).send({ message: "Recaptcha failed" });
+    return;
+  }
 }
 
-export const adminRoutes = async(fastify: FastifyInstance, _options: RegisterOptions) => {
-    fastify.addHook("preHandler", fastify.auth([verifyAdmin, verifyRecaptcha]));
-    fastify.get("/users", getUsers);
-    fastify.post("/create-user", {
-            schema: {
-                body: {
-                    type: "object",
-                    properties: {
-                        username: { type: "string" },
-                        email: { type: "string" },
-                        password: { type: "string" },
-                        pictureUrl: { type: "string" },
-                        isAdmin: { type: "number" }
-                    },
-                    required: ["username", "email", "password"]
-                }
-            }
-        },
-        createAdminOriginUser);
-}
+export const adminRoutes = async (fastify: FastifyInstance, _options: RegisterOptions) => {
+  fastify.addHook("preHandler", fastify.auth([verifyAdmin, verifyRecaptcha]));
+  fastify.get("/users", getUsers);
+  fastify.post(
+    "/create-user",
+    {
+      schema: {
+        body: {
+          type: "object",
+          properties: {
+            username: { type: "string" },
+            email: { type: "string" },
+            password: { type: "string" },
+            pictureUrl: { type: "string" },
+            isAdmin: { type: "number" }
+          },
+          required: ["username", "email", "password"]
+        }
+      }
+    },
+    adminCreateUser
+  );
+  fastify.post(
+    "/delete-user",
+    {
+      schema: {
+        body: {
+          type: "object",
+          properties: {
+            uuid: { type: "string" }
+          },
+          required: ["uuid"]
+        }
+      }
+    },
+    adminDeleteUser
+  );
+};
--- server/src/admin-routes.ts
+++ server/src/admin-routes.ts
@@ -1,50 +1,95 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { checkForUsersWithUsernameOrEmail, createUser, findUser } from "./database";
+import { checkForUsersWithUsernameOrEmail, createUser, findUser } from "./database.js";
 import { randomUUID } from "crypto";
 import { hash } from "bcrypt-ts";
-import { emailTestRegex } from "./login-routes";
+import { emailTestRegex } from "./login-routes.js";
+import { deleteUser } from "./database.js";
+import path from "path";
+import { assetconfigs } from "./util.js";
+import { realpathSync } from "fs";
+import { rm, writeFile } from "fs/promises";
+import { loadFileMetadata } from "./file-metadata-handler.js";
 
 export const getUsers = async function getUsers(_request: FastifyRequest, reply: FastifyReply) {
-    const users = await findUser({});
-    reply.send(users);
-}
+  const users = await findUser({});
+  reply.send(users);
+};
 
-export const createAdminOriginUser = async function createAdminOriginUser(request: FastifyRequest, reply: FastifyReply) {
-    const { username, email, password, pictureUrl, isAdmin } = request.body as {
-        username: string;
-        email: string;
-        password: string;
-        pictureUrl: string;
-        isAdmin: number;
+export const adminCreateUser = async function adminCreateUser(request: FastifyRequest, reply: FastifyReply) {
+  const { username, email, password, pictureUrl, isAdmin } = request.body as {
+    username: string;
+    email: string;
+    password: string;
+    pictureUrl: string;
+    isAdmin: number;
+  };
+  if (!username || !email || !password || !emailTestRegex.test(email)) {
+    reply.status(400).send({ message: "Missing/invalid required fields" });
+    return;
+  }
+  try {
+    const user = await checkForUsersWithUsernameOrEmail(username, email);
+    if (user.length > 0) {
+      reply.status(409).send({ message: "User already exists" });
+      return;
+    }
+    const customUser = {
+      username,
+      email,
+      password: await hash(password, 10),
+      uuid: randomUUID(),
+      pictureUrl: pictureUrl ? pictureUrl : null,
+      isAdmin: isAdmin ? 1 : 0,
+      emailVerified: 1,
+      invalidateSession: 0
     };
-    if (!username || !email || !password || !emailTestRegex.test(email)) {
-        reply.status(400).send({ message: "Missing/invalid required fields" });
-        return;
+    await createUser(customUser);
+    const newUser = await findUser({ username });
+    if (newUser.length !== 1) {
+      throw new Error("User not created");
+    }
+  } catch (error) {
+    reply.status(500).send({ message: "Database error" });
+    return;
+  }
+  reply.send({ message: "User created" });
+};
+
+export const adminDeleteUser = async function adminDeleteUser(
+  request: FastifyRequest<{ Body: { uuid: string } }>,
+  reply: FastifyReply
+) {
+  const { uuid } = request.body;
+  if (!uuid) {
+    reply.status(400).send({ message: "Missing uuid" });
+    return;
+  }
+  try {
+    const user = await findUser({ uuid });
+    if (!user) {
+      reply.status(404).send({ message: "User not found" });
+      return;
+    }
+    await deleteUser({ uuid });
+    const userDirectory = realpathSync(path.resolve(assetconfigs.uploadDirectory, uuid));
+    if (!userDirectory.startsWith(assetconfigs.uploadDirectory)) {
+      throw new Error("Directory traversal detected");
     }
     try {
-        const user = await checkForUsersWithUsernameOrEmail( username, email );
-        if (user.length > 0) {
-            reply.status(409).send({ message: "User already exists" });
-            return;
-        }
-        const customUser = {
-            username,
-            email,
-            password: await hash(password, 10),
-            uuid: randomUUID(),
-            pictureUrl: pictureUrl ? pictureUrl : null,
-            isAdmin: isAdmin ? 1 : 0,
-            emailVerified: 1,
-            invalidateSession: 0
-        }
-        await createUser(customUser);
-        const newUser = await findUser({ username });
-        if (newUser.length !== 1) {
-            throw new Error("User not created");
-        }
+      await rm(userDirectory, { recursive: true, force: true });
     } catch (error) {
-        reply.status(500).send({ message: "Database error" });
-        return;
+      console.error(error);
     }
-    reply.send({ message: "User created" });
-}
+    const metadata = await loadFileMetadata(assetconfigs.fileMetadata);
+    for (const file in metadata) {
+      if (file.includes(uuid)) {
+        delete metadata[file];
+      }
+    }
+    await writeFile(assetconfigs.fileMetadata, JSON.stringify(metadata));
+  } catch (error) {
+    reply.status(500).send({ message: "Unknown error" });
+    return;
+  }
+  reply.send({ message: "User deleted" });
+};
