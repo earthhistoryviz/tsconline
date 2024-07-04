e5336c31f5d87f5970f7109627bf78138e686b9a
asivath <asivatha@purdue.edu>
2024-07-03 20:44:18 -0400

    updates

--- preview.sh
+++ preview.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose exec -T pr-preview bash -c "source /root/.nvm/nvm.sh && nvm use node && yarn && yarn build"
-docker-compose up -d pr-preview
+docker-compose -f docker-compose-preview.yml exec -T pr-preview bash -c "source /root/.nvm/nvm.sh && nvm use node && yarn && yarn build"
+docker-compose -f docker-compose-preview.yml up -d pr-preview
