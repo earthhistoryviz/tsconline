b84465b1ed4d10a15e8a5e0c0a6d033a38009ace
asivath <asivatha@purdue.edu>
2024-07-04 17:04:19 -0400

    changes

--- preview.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp ../server/assets/jars/* ./server/assets/jars/
-cp ../server/assets/datapacks/* ./server/assets/datapacks/
-docker-compose -f docker-compose-preview.yml up -d pr-preview
