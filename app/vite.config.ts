df144b4733319e8ece881d9971ab542bef650ffb
asivath <asivatha@purdue.edu>
2024-07-04 12:20:02 -0400

    changes

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/app/",
   plugins: [react()],
   server: {
     open: true,
