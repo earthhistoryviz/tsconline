1a84b803cb99acd3dd87c6c38b33d3838dcfda7a
Changk820 <changk7973@gmail.com>
2024-07-10 04:36:44 -0400

    bruh seriously?

--- app/src/Home.tsx
+++ app/src/Home.tsx
@@ -9,7 +9,7 @@ import { Accordion, AccordionSummary, AccordionDetails, Grid, Typography } from 
 import { useTheme, styled } from "@mui/material/styles";
 import { TSCIcon, TSCButton, TSCCard, StyledScrollbar } from "./components";
 import TSCreatorLogo from "./assets/TSCreatorLogo.png";
-import CookieConsent from "./CookieConsent";
+// import CookieConsent from "./CookieConsent";
 import "./Home.css";
 
 const HeaderContainer = styled("div")(({ theme }) => ({
