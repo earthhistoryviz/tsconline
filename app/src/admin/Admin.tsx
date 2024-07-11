7bc663a759b51e37a6dd89792ee7fedae2a5c0b1
Paolo Gumasing <paologum@gmail.com>
2024-07-11 17:09:48 -0400

    added unauth access page

--- app/src/admin/Admin.tsx
+++ app/src/admin/Admin.tsx
@@ -1,5 +1,10 @@
 import { observer } from "mobx-react-lite";
+import { useContext } from "react";
+import { context } from "../state";
+import { UnauthorizedAccess } from "./UnauthorizedAccess";
 
 export const Admin = observer(function Admin() {
+  const { state } = useContext(context);
+  if (!state.user.isAdmin) return <UnauthorizedAccess />;
   return <div>Admin</div>;
 });
