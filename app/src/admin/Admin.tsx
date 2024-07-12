c660ba3a6bc09718b61fe5419da63938da2ee5b8
Paolo Gumasing <paologum@gmail.com>
2024-07-12 03:26:05 -0400

    working grid

--- app/src/admin/Admin.tsx
+++ app/src/admin/Admin.tsx
@@ -1,18 +1,31 @@
 import { observer } from "mobx-react-lite";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { context } from "../state";
 import { UnauthorizedAccess } from "./UnauthorizedAccess";
+import { loadRecaptcha, removeRecaptcha } from "../util";
 import { AgGridReact } from "ag-grid-react";
-import { executeRecaptcha, fetcher, loadRecaptcha, removeRecaptcha } from "../util";
-import { User } from "../types";
-import { ErrorCodes } from "../util/error-codes";
-import { Typography } from "@mui/material";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
+import { ColDef } from "ag-grid-community";
+
+const colDefs: ColDef[] = [
+  { headerName: "Username", field: "username" },
+  { headerName: "Email", field: "email" },
+  { headerName: "UUID", field: "uuid" },
+  { headerName: "User ID", field: "userId" },
+  { headerName: "Is a Google User", field: "isGoogleUser" },
+  { headerName: "Invalidated Sessions", field: "invalidateSession" },
+  { headerName: "Email Verified", field: "emailVerified" },
+  { headerName: "Picture URL", field: "pictureUrl" },
+  { headerName: "Is Admin", field: "isAdmin" }
+];
 
 export const Admin = observer(function Admin() {
   const { state, actions } = useContext(context);
   useEffect(() => {
-    loadRecaptcha().then(() => {
-      actions.fetchUsers();
+    loadRecaptcha().then(async () => {
+      await actions.fetchUsers();
+      console.log(JSON.stringify(state.admin.displayedUsers, null, 2));
     });
     return () => {
       removeRecaptcha();
@@ -20,10 +33,13 @@ export const Admin = observer(function Admin() {
   }, []);
   if (!state.user.isAdmin) return <UnauthorizedAccess />;
   return (
-    <div>
-      {state.admin.displayedUsers.map((user) => (
-        <Typography key={user.uuid}>{user.username}</Typography>
-      ))}
+    <div className="ag-theme-quartz" style={{ height: 500 }}>
+      <AgGridReact
+        columnDefs={colDefs}
+        rowData={state.admin.displayedUsers}
+        pagination={true}
+        paginationPageSize={20}
+      />
     </div>
   );
 });
