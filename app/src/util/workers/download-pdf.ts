c4b6edd01770beebfcd6dee275c3fe51fb3cf079
jay-jhk-lee <jaylee.62893@gmail.com>
2024-07-12 16:51:16 -0400

    added timeout for save chart as pdf

--- app/src/util/workers/download-pdf.ts
+++ app/src/util/workers/download-pdf.ts
@@ -1,6 +1,6 @@
 import jsPDF from "jspdf";
-import { DownloadPdfMessage } from "../../types";
-self.onmessage = (e: MessageEvent<DownloadPdfMessage>) => {
+import { DownloadPdfCompleteMessage, DownloadPdfMessage } from "../../types";
+self.onmessage = async (e: MessageEvent<DownloadPdfMessage>) => {
   const { imgURI, height, width } = e.data;
   const orientation = height > width ? "portrait" : "landscape";
   const doc = new jsPDF({
@@ -9,7 +9,30 @@ self.onmessage = (e: MessageEvent<DownloadPdfMessage>) => {
     format: [height, width],
     compress: true
   });
-  doc.addImage(imgURI, "png", 0, 0, width, height);
-  const blob = doc.output("blob");
-  self.postMessage(blob);
+  //this can take a long time, so set a timeout to not block the save chart functionality
+  const addImage = new Promise((resolve, _) => {
+    doc.addImage(imgURI, "png", 0, 0, width, height);
+    resolve("Chart Added to PDF in download pdf worker");
+  });
+  //ms
+  const timeoutThreshold = 30000;
+  const timeoutPromise = new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error("Function timed out after" + timeoutThreshold + " ms"));
+    }, timeoutThreshold);
+  });
+
+  const message: DownloadPdfCompleteMessage = { status: "success", value: undefined };
+  async function runWithTimeout() {
+    try {
+      const result = await Promise.race([addImage, timeoutPromise]);
+      console.log(result);
+      message.value = doc.output("blob");
+    } catch (error) {
+      console.log(error);
+      message.status = "failure";
+    }
+  }
+  await runWithTimeout();
+  self.postMessage(message);
 };
