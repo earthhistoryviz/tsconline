d25e130947fc2fa0d87db81a3c2bda0c328cafc5
Paolo Gumasing <paologum@gmail.com>
2024-07-12 22:57:27 -0400

    added admin header customizations

--- app/src/admin/Admin.tsx
+++ app/src/admin/Admin.tsx
@@ -7,21 +7,29 @@ import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 import { ColDef } from "ag-grid-community";
+import { useTheme } from "@mui/material";
 
 const colDefs: ColDef[] = [
-  { headerName: "Username", field: "username" },
-  { headerName: "Email", field: "email" },
+  { headerName: "Username", field: "username", sortable: true, filter: true },
+  { headerName: "Email", field: "email", sortable: true, filter: true },
   { headerName: "UUID", field: "uuid" },
-  { headerName: "User ID", field: "userId" },
-  { headerName: "Is a Google User", field: "isGoogleUser" },
-  { headerName: "Invalidated Sessions", field: "invalidateSession" },
-  { headerName: "Email Verified", field: "emailVerified" },
-  { headerName: "Picture URL", field: "pictureUrl" },
-  { headerName: "Is Admin", field: "isAdmin" }
+  { headerName: "User ID", field: "userId", width: 80 },
+  { headerName: "Google User", field: "isGoogleUser", width: 120 },
+  {
+    headerName: "Invalidated Session?",
+    field: "invalidateSession",
+    width: 110,
+    autoHeaderHeight: true,
+    wrapHeaderText: true
+  },
+  { headerName: "Email Verified", field: "emailVerified", width: 90, autoHeaderHeight: true, wrapHeaderText: true },
+  { headerName: "Picture URL", field: "pictureUrl", width: 80, autoHeaderHeight: true, wrapHeaderText: true },
+  { headerName: "Is Admin", field: "isAdmin", width: 90, autoHeaderHeight: true, wrapHeaderText: true }
 ];
 
 export const Admin = observer(function Admin() {
   const { state, actions } = useContext(context);
+  const theme = useTheme();
   useEffect(() => {
     loadRecaptcha().then(async () => {
       await actions.fetchUsers();
@@ -33,7 +41,7 @@ export const Admin = observer(function Admin() {
   }, []);
   if (!state.user.isAdmin) return <UnauthorizedAccess />;
   return (
-    <div className="ag-theme-quartz" style={{ height: 500 }}>
+    <div className={theme.palette.mode === "dark" ? "ag-theme-quartz-dark" : "ag-theme-quartz"} style={{ height: 500 }}>
       <AgGridReact
         columnDefs={colDefs}
         rowData={state.admin.displayedUsers}
