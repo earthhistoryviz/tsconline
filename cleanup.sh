8797993dc26c202adcdaf5b7b5d0b322f13e1820
asivath <asivatha@purdue.edu>
2024-07-13 15:00:13 -0400

    updates to pipelines

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PR_ID=$1
-cd /home/aaron/live
-rm -f proxy/user_conf.d/pr-${PR_ID}.conf
-docker-compose restart proxy
-cd /home/aaron/tsconline/pr-preview-${PR_ID}
-docker-compose -f docker-compose-preview.yml down
-cd ..
-sudo rm -rf pr-preview-${PR_ID}
