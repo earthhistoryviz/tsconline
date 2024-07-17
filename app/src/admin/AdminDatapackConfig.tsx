0d084fda2a02f0f392a91de52ec4f86201a9d0fa
Paolo Gumasing <paologum@gmail.com>
2024-07-17 15:02:28 -0400

    added tabs to switch between user and datapack config

---
+++ app/src/admin/AdminDatapackConfig.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from "@mui/material";
+import { observer } from "mobx-react-lite";
+
+export const AdminDatapackConfig = observer(function AdminDatapackConfig() {
+  return (
+    <Box>
+      <Typography variant="body1">Coming soon!</Typography>
+    </Box>
+  );
+});
