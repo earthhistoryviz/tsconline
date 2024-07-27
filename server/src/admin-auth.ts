3c565efb7f3df9b11222b38cc4ede70782aa4b40
asivath <asivatha@purdue.edu>
2024-07-27 16:21:07 -0400

    fix

--- server/src/admin-auth.ts
+++ server/src/admin-auth.ts
@@ -12,7 +12,6 @@ import {
 import { checkRecaptchaToken } from "./verify.js";
 import { googleRecaptchaBotThreshold } from "./login-routes.js";
 
-
 async function verifyAdmin(request: FastifyRequest, reply: FastifyReply) {
   const uuid = request.session.get("uuid");
   if (!uuid) {
