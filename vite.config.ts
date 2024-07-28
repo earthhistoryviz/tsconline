498c40a7d2184e86987ca558d2560d60d4850497
asivath <asivatha@purdue.edu>
2024-07-28 11:59:04 -0400

    change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    maxConcurrency: 20,
+    pool: "vmThreads",
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      }
+    },
+    isolate: false,
     cache: false,
     environment: "node",
     include: ["server/__tests__/**.test.ts", "app/__tests__/**.test.ts", "shared/__tests__/**.test.ts"],
