213799379dcab19f9cf124920238780b5470a526
Jacqui <li4584@purdue.edu>
2024-07-29 00:58:50 -0400

    prettier

--- app/src/settings_tabs/map_points/MapPoints.tsx
+++ app/src/settings_tabs/map_points/MapPoints.tsx
@@ -80,7 +80,7 @@ const MapList: React.FC<MapRowComponentProps> = observer(({ mapInfo }) => {
       <Box>
         <List>
           {Object.entries(sortedMapInfoObj).map(([name, map]) => {
-            console.log("name: " + name, " ,parent: " + map.parent?.name)
+            console.log("name: " + name, " ,parent: " + map.parent?.name);
             return (
               <MapListItemButton
                 disableRipple
