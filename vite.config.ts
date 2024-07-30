b84fc9390ed35cad689c40ca2c66eed4695a4d40
asivath <asivatha@purdue.edu>
2024-07-30 16:44:38 -0400

    config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vitest/config";
 
+const config = {
+  statements: 80,
+  branches: 80,
+  functions: 80,
+  lines: 80
+}
+
 export default defineConfig({
   test: {
     maxConcurrency: 20,
@@ -9,7 +16,17 @@ export default defineConfig({
     include: ["server/__tests__/**.test.ts", "app/__tests__/**.test.ts", "shared/__tests__/**.test.ts"],
     coverage: {
       reporter: ["text", "json-summary", "json", "lcov"],
-      include: ["server/src/**", "app/src/**", "shared/src/**"]
+      include: ["server/src/**", "app/src/**", "shared/src/**"],
+      thresholds: {
+        "server/src/admin-auth.ts": config,
+        "server/src/admin-routes.ts": config,
+        "server/src/encryption.ts": config,
+        "server/src/login-routes.ts": config,
+        "server/src/parse-datapacks.ts": config,
+        "server/src/parse-map-packs.ts": config,
+        "shared/src/util.ts": config
+      },
+      ignoreEmptyLines: true
     },
     outputFile: "coverage/sonar-report.xml"
   },
