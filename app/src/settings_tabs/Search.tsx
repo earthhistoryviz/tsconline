ad6d3fe08c864750a27308b1d6bb97e12232fc07
jay-jhk-lee <jaylee.62893@gmail.com>
2024-08-10 16:12:13 -0400

    changed time display to be a table

--- app/src/settings_tabs/Search.tsx
+++ app/src/settings_tabs/Search.tsx
@@ -1,35 +1,53 @@
-import { TextField, Typography } from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
 import { observer } from "mobx-react-lite";
 import { useContext, useRef } from "react";
 import "./Search.css";
 import { context } from "../state";
 import { Results } from "./Results";
+import { EventSearchInfo, GroupedEventSearchInfo } from "../types";
+import { StyledScrollbar } from "../components";
 export const Search = observer(function Search() {
   const { state, actions } = useContext(context);
   const countRef = useRef(0);
   const TimeDisplay = observer(() => {
     return (
-      <div className="search-time-display-container">
-        <Typography>Current Time Settings (Top / Base)</Typography>
-        <div className="search-time-display-ages-container">
-          <div className="time-display-resize-wrapper">
-            {Object.keys(state.settings.timeSettings).map((unit) => (
-              <div className="search-time-display-ages" key={unit}>
-                <Typography width={"50%"}>{unit}:</Typography>
-                <Typography>
-                  {state.settings.timeSettings[unit].topStageAge} / {state.settings.timeSettings[unit].baseStageAge}
-                </Typography>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <TableContainer component={Paper} className="search-time-display-container">
+        <StyledScrollbar>
+          <Table stickyHeader size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ padding: 0 }} align="center" colSpan={2}>
+                  Current Time Settings
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ padding: "0 16px" }} align="left">
+                  Unit
+                </TableCell>
+                <TableCell sx={{ padding: "0 16px" }} align="right">
+                  Top/Base
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.keys(state.settings.timeSettings).map((unit) => (
+                <TableRow key={unit} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                  <TableCell>{unit}</TableCell>
+                  <TableCell align="right">
+                    {state.settings.timeSettings[unit].topStageAge}/{state.settings.timeSettings[unit].baseStageAge}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </StyledScrollbar>
+      </TableContainer>
     );
   });
 
   return (
     <div className="search-container">
-      <div style={{ display: "flex", flexDirection: "row" }}>
+      <div className="search-header-container">
         <div className="search-and-options">
           <TextField
             className="search-bar"
