d60997ab0293cbfc4f37d8bda3aebc8c7ac768e8
asivath <asivatha@purdue.edu>
2024-08-18 09:47:32 -0400

    added error codes for chart generation failure

--- app/src/util/error-codes.ts
+++ app/src/util/error-codes.ts
@@ -66,7 +66,9 @@ export enum ErrorCodes {
   INVALID_SERVER_DATAPACK_REQUEST = "INVALID_SERVER_DATAPACK_REQUEST",
   SERVER_FILE_METADATA_ERROR = "SERVER_FILE_METADATA_ERROR",
   USER_DELETE_DATAPAACK_FAILED = "USER_DELETE_DATAPACK_FAILED",
-  UNABLE_TO_PROCESS_DATAPACK_CONFIG = "UNABLE_TO_PROCESS_DATAPACK_CONFIG"
+  UNABLE_TO_PROCESS_DATAPACK_CONFIG = "UNABLE_TO_PROCESS_DATAPACK_CONFIG",
+  INVALID_SETTINGS = "INVALID_SETTINGS",
+  INTERNAL_ERROR = "INTERNAL_ERROR"
 }
 
 export const ErrorMessages = {
@@ -145,5 +147,7 @@ export const ErrorMessages = {
   [ErrorCodes.INVALID_SERVER_DATAPACK_REQUEST]: "Invalid server datapack request. Please try again later.",
   [ErrorCodes.SERVER_FILE_METADATA_ERROR]: "Server file metadata error. Please try again later.",
   [ErrorCodes.USER_DELETE_DATAPAACK_FAILED]: "Unable to delete user datapack. Please try again later.",
-  [ErrorCodes.UNABLE_TO_PROCESS_DATAPACK_CONFIG]: "falied to process datapack config. Please try again later."
+  [ErrorCodes.UNABLE_TO_PROCESS_DATAPACK_CONFIG]: "falied to process datapack config. Please try again later.",
+  [ErrorCodes.INVALID_SETTINGS]: "Invalid settings. Please try again.",
+  [ErrorCodes.INTERNAL_ERROR]: "There was an internal error while generating the chart. Please try again later."
 };
