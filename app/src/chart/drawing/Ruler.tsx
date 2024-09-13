be275d796b5c83f94ef48e19783c9c38472736cc
paologum <pgumasin@purdue.edu>
2024-09-13 21:24:28 +0900

    added some performance optimzations

--- app/src/chart/drawing/Ruler.tsx
+++ app/src/chart/drawing/Ruler.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Layer, Line, Rect, Text } from "react-konva";
+import React, { useCallback, useMemo } from "react";
+import { FastLayer, Group, Layer, Line, Rect, Text } from "react-konva";
 import { TimeSettings } from "../../types";
 
 interface RulerProps {
@@ -52,41 +52,52 @@ const Ruler: React.FC<RulerProps> = ({ width, height, x, y, timeSettings, mirror
     majorInterval *= 5;
     labelInterval = majorInterval;
   }
+  const handleWidthChange = useCallback((width: number) => {
+    setTimeout(() => {
+      setRulerWidth((prevWidth) => Math.max(prevWidth, width));
+    }, 100); // Debouncing by 100ms
+  }, []);
   // Draw ticks
-  const scaleStep = minorInterval < 0 ? majorInterval : minorInterval;
-  let tickMY = timeSettings.topStageAge + scaleStep;
-  const ticks = [];
-  // this function is used to check if a number is a multiple of another number given a tolerance
-  const isMultiple = (num: number, interval: number, tolerance = Number.EPSILON) => {
-    const remainder = num % interval;
-    // we do modular again since the fixed rounding can be imprecise the second time
-    return parseFloat(remainder.toFixed(2)) % interval < tolerance;
-  };
-  while (tickMY <= timeSettings.baseStageAge) {
-    tickMY = parseFloat(tickMY.toFixed(10));
-    const isMajor = isMultiple(tickMY, majorInterval);
-    const isIntermediate = tickMY % labelInterval === 0;
-    const isMinor = isMultiple(tickMY, minorInterval);
-    const tickX = mirrored ? x + rulerWidth : x;
-    const tickY = (tickMY - timeSettings.topStageAge) * scale + y;
+  const ticks = useMemo(() => {
+    const scaleStep = minorInterval < 0 ? majorInterval : minorInterval;
+    let tickMY = timeSettings.topStageAge + scaleStep;
+    const ticks = [];
+    // this function is used to check if a number is a multiple of another number given a tolerance
+    const isMultiple = (num: number, interval: number, tolerance = Number.EPSILON) => {
+      const remainder = num % interval;
+      // we do modular again since the fixed rounding can be imprecise the second time
+      return parseFloat(remainder.toFixed(2)) % interval < tolerance;
+    };
+    while (tickMY <= timeSettings.baseStageAge) {
+      tickMY = parseFloat(tickMY.toFixed(10));
+      const isMajor = isMultiple(tickMY, majorInterval);
+      const isIntermediate = isMultiple(tickMY, intermediateInterval);
+      const isMinor = isMultiple(tickMY, minorInterval);
+      const tickX = mirrored ? x + rulerWidth : x;
+      const tickY = (tickMY - timeSettings.topStageAge) * scale + y;
 
-    if (isMajor) {
-      ticks.push({ x: tickX, y: tickY, type: "major", value: tickMY });
-    } else if (isMinor) {
-      ticks.push({ x: tickX, y: tickY, type: "minor", value: tickMY });
-    } else {
+      if (isMajor) {
+        ticks.push({ x: tickX, y: tickY, type: "major", value: tickMY });
+      } else if (isMinor) {
+        ticks.push({ x: tickX, y: tickY, type: "minor", value: tickMY });
+      } else if (isIntermediate) {
+        ticks.push({ x: tickX, y: tickY, type: "intermediate", value: tickMY });
+      }
+
+      tickMY += scaleStep;
     }
-
-    tickMY += scaleStep;
-  }
+    return ticks;
+  }, [timeSettings, scale, x, y, rulerWidth, mirrored, majorInterval, minorInterval, intermediateInterval]);
 
   return (
-    <Layer clipX={x} clipY={y} clipWidth={rulerWidth} clipHeight={height}>
+    <FastLayer clipX={x} clipY={y} clipWidth={rulerWidth} clipHeight={height}>
       <Rect x={x} y={y} width={rulerWidth} height={height} stroke="black" />
+      <Group>
       {ticks.map((tick, index) => {
+        const offsetX = tick.type === "major" ? MAJOR_TICK_LENGTH : tick.type === "intermediate" ? INTERM_TICK_LENGTH : MINOR_TICK_LENGTH;
         const points = mirrored
-          ? [tick.x, tick.y, tick.x - (tick.type === "major" ? MAJOR_TICK_LENGTH : MINOR_TICK_LENGTH), tick.y]
-          : [tick.x, tick.y, tick.x + (tick.type === "major" ? MAJOR_TICK_LENGTH : MINOR_TICK_LENGTH), tick.y];
+          ? [tick.x, tick.y, tick.x - offsetX, tick.y]
+          : [tick.x, tick.y, tick.x + offsetX, tick.y];
         // this is the x position of the label, if mirrored it will be on the left side of the tick, otherwise on the right
         const maxTickLength = MAJOR_TICK_LENGTH + LABEL_PADDING;
         return (
@@ -101,12 +112,13 @@ const Ruler: React.FC<RulerProps> = ({ width, height, x, y, timeSettings, mirror
                 fill="black"
                 align={mirrored ? "right" : "left"}
                 ref={(node) => {
+                  // this is used to calculate the width of the ruler and to offset the x position of the node if it is mirrored
                   if (node) {
                     if (mirrored) {
                       node.offsetX(node.width());
                     }
                     if (maxTickLength + node.width() + OUTER_LABEL_PADDING >= rulerWidth) {
-                      setRulerWidth(maxTickLength + node.width() + OUTER_LABEL_PADDING);
+                      handleWidthChange(maxTickLength + node.width() + OUTER_LABEL_PADDING);
                     }
                   }
                 }}
@@ -116,7 +128,8 @@ const Ruler: React.FC<RulerProps> = ({ width, height, x, y, timeSettings, mirror
           </React.Fragment>
         );
       })}
-    </Layer>
+      </Group>
+    </FastLayer>
   );
 };
 
