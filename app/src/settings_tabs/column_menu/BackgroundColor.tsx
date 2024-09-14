40f20b10574384799bc22f1683d0cfaea0703737
Jacqui <li4584@purdue.edu>
2024-09-14 19:31:10 -0400

    add hook back

--- app/src/settings_tabs/column_menu/BackgroundColor.tsx
+++ app/src/settings_tabs/column_menu/BackgroundColor.tsx
@@ -6,20 +6,21 @@ import "./BackgroundColor.css";
 import { convertHexToRGB } from "../../util/util";
 import { ColumnInfo } from "@tsconline/shared";
 import { context } from "../../state";
+import { useTranslation } from "react-i18next";
 
 interface ChangeBGColorProps {
   column: ColumnInfo;
-  text: string;
 }
-export const ChangeBackgroundColor: React.FC<ChangeBGColorProps> = observer(({ column, text }) => {
+export const ChangeBackgroundColor: React.FC<ChangeBGColorProps> = observer(({ column }) => {
   const { actions } = useContext(context);
+  const { t } = useTranslation();
   const handleColorChange = (color: string) => {
     actions.setRGB(convertHexToRGB(color, false), column);
   };
 
   return (
     <div>
-      <FormLabel>{text}:&nbsp;</FormLabel>
+      <FormLabel>{t("settings.column.menu.background-color")}:&nbsp;</FormLabel>
       <TSCColorPicker
         key={column.name}
         color={`rgb(${column.rgb.r}, ${column.rgb.g}, ${column.rgb.b})`}
