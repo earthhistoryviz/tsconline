099e8fe912210f8f0528f90ac1be41196689d527
Jacqui <li4584@purdue.edu>
2024-09-14 19:35:02 -0400

    fogot to save change

--- app/src/settings_tabs/column_menu/EditNameField.tsx
+++ app/src/settings_tabs/column_menu/EditNameField.tsx
@@ -4,15 +4,16 @@ import { context } from "../../state";
 import { TextField, Typography } from "@mui/material";
 import "./ColumnMenu.css";
 import { ColumnInfo } from "@tsconline/shared";
+import { useTranslation } from "react-i18next";
 
 export const EditNameField: React.FC<{
   column: ColumnInfo;
-  text: string;
-}> = observer(({ column, text }) => {
+}> = observer(({ column }) => {
   const { actions } = useContext(context);
+  const { t } = useTranslation();
   return (
     <div>
-      <Typography id="edit-name-text">{text}</Typography>
+      <Typography id="edit-name-text">{t("settings.column.menu.edit-name")}</Typography>
       <TextField
         hiddenLabel
         fullWidth
