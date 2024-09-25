a2e77e0aee24ff2d7cf2228293da3b9e40142aef
paologum <pgumasin@purdue.edu>
2024-09-26 02:46:12 +0900

    added submission logic

--- app/src/state/actions/user-actions.ts
+++ app/src/state/actions/user-actions.ts
@@ -4,6 +4,38 @@ import { getRecaptchaToken, pushError, pushSnackbar } from "./general-actions";
 import { displayServerError } from "./util-actions";
 import { ErrorCodes, ErrorMessages } from "../../util/error-codes";
 import { state } from "../state";
+import { EditableDatapackMetadata } from "../../types";
+
+export const handleDatapackEdit = action(async (datapack: EditableDatapackMetadata) => {
+  const formData = new FormData();
+  for (const key in datapack) {
+    const value = datapack[key as keyof EditableDatapackMetadata];
+    if (value !== undefined) formData.append(key, String(value));
+  }
+  try {
+    const recaptcha = await getRecaptchaToken("handleDatapackEdit");
+    if (!recaptcha) return;
+    const response = await fetcher(`/user/datapack/${datapack.title}`, {
+      method: "PATCH",
+      body: formData,
+      credentials: "include",
+      headers: {
+        "recaptcha-token": recaptcha
+      }
+    });
+    if (response.ok) {
+      pushSnackbar("Datapack updated", "success");
+    } else {
+      displayServerError(
+        response,
+        ErrorCodes.USER_EDIT_DATAPACK_FAILED,
+        ErrorMessages[ErrorCodes.USER_EDIT_DATAPACK_FAILED]
+      );
+    }
+  } catch (e) {
+    pushError(ErrorCodes.SERVER_RESPONSE_ERROR);
+  }
+});
 
 export const userDeleteDatapack = action(async (datapack: string) => {
   try {
