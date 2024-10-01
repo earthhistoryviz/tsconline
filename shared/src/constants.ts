a425f633ffee7e11f4a65a03dbb4fb2f7a8141a6
jay-jhk-lee <jaylee.62893@gmail.com>
2024-10-01 09:54:43 +0200

    changed property from boolean to string for referencing

--- shared/src/constants.ts
+++ shared/src/constants.ts
@@ -56,7 +56,7 @@ export const defaultEventSettings: EventSettings = {
   type: "events",
   rangeSort: "first occurrence",
   frequency: null,
-  isDualColCompColumn: false,
+  dualColCompColumnRef: null,
   drawDualColCompColumn: null,
   ...defaultDataMiningSettings
 };
@@ -440,7 +440,7 @@ export const defaultPointSettings: PointSettings = {
   maxX: Number.MIN_SAFE_INTEGER,
   dataMiningPointDataType: null,
   isDataMiningColumn: false,
-  isDualColCompColumn: false,
+  dualColCompColumnRef: null,
   drawDualColCompColumn: null,
   ...defaultDataMiningSettings
 };
