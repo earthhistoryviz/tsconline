013b824876a6e2ca22f0c63b551c25c83c112c25
jay-jhk-lee <jaylee.62893@gmail.com>
2024-10-03 21:28:01 +0200

    added correct parsing

--- app/src/state/parse-settings.ts
+++ app/src/state/parse-settings.ts
@@ -414,7 +414,9 @@ export function translateColumnInfoToColumnInfoTSC(state: ColumnInfo): ColumnInf
         rangeSort: state.columnSpecificSettings.rangeSort,
         drawExtraColumn: state.columnSpecificSettings.frequency,
         windowSize: state.columnSpecificSettings.windowSize,
-        stepSize: state.columnSpecificSettings.stepSize
+        stepSize: state.columnSpecificSettings.stepSize,
+        isDualColCompColumn: state.columnSpecificSettings.dualColCompColumnRef ? true : false,
+        drawDualColCompColumn: state.columnSpecificSettings.drawDualColCompColumn
       };
       break;
     case "Zone":
@@ -482,7 +484,9 @@ export function translateColumnInfoToColumnInfoTSC(state: ColumnInfo): ColumnInf
         drawExtraColumn: state.columnSpecificSettings.dataMiningPointDataType,
         windowSize: state.columnSpecificSettings.windowSize,
         stepSize: state.columnSpecificSettings.stepSize,
-        isDataMiningColumn: state.columnSpecificSettings.isDataMiningColumn
+        isDataMiningColumn: state.columnSpecificSettings.isDataMiningColumn,
+        isDualColCompColumn: state.columnSpecificSettings.dualColCompColumnRef ? true : false,
+        drawDualColCompColumn: state.columnSpecificSettings.drawDualColCompColumn
       };
       break;
     case "Chron":
@@ -650,7 +654,9 @@ export function columnInfoTSCToXml(column: ColumnInfoTSC, indent: string): strin
       for (const child of column.children) {
         const isDataMiningColumn =
           "isDataMiningColumn" in child ? `isDataMiningColumn="${child.isDataMiningColumn}"` : "";
-        xml += `${indent}<column id="${escapeHtmlChars(child._id, "attribute")}" ${isDataMiningColumn}>\n`;
+        const isDualColCompColumn =
+          "isDualColCompColumn" in child ? `isDualColCompColumn="${child.isDualColCompColumn}"` : "";
+        xml += `${indent}<column id="${escapeHtmlChars(child._id, "attribute")}" ${isDataMiningColumn} ${isDualColCompColumn}>\n`;
         xml += columnInfoTSCToXml(child, `${indent}    `);
         xml += `${indent}</column>\n`;
       }
@@ -670,6 +676,9 @@ export function columnInfoTSCToXml(column: ColumnInfoTSC, indent: string): strin
       xml += `${indent}<setting name="type" type="${column.type}"/>\n`;
     } else if (isRGB(keyValue)) {
       xml += `${indent}<setting name="${key}">${convertRgbToString(keyValue)}</setting>\n`;
+    } else if (key === "drawDualColCompColumn") {
+      if (typeof keyValue !== "string") continue;
+      xml += `${indent}<setting name="${key}">${escapeHtmlChars(keyValue, "attribute")}</setting>\n`;
     } else {
       xml += `${indent}<setting name="${key}">${keyValue}</setting>\n`;
     }
