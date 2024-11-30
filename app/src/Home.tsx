ea5a3510685c6a8fb6abe6b8ea59b341b630810c
paologum <pgumasin@purdue.edu>
2024-11-30 17:42:23 +0900

    add initial landing page rework

--- app/src/Home.tsx
+++ app/src/Home.tsx
@@ -5,19 +5,20 @@ import { NavigateFunction, useNavigate } from "react-router-dom";
 import { ChartConfig, DatapackConfigForChartRequest, assertDatapackConfigForChartRequest } from "@tsconline/shared";
 import { context } from "./state";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Accordion, AccordionSummary, AccordionDetails, Grid, Typography } from "@mui/material";
+import { Accordion, AccordionSummary, AccordionDetails, Grid, Typography, Box } from "@mui/material";
 import { useTheme, styled } from "@mui/material/styles";
-import { TSCIcon, TSCButton, TSCCard, StyledScrollbar } from "./components";
-import TSCreatorLogo from "./assets/TSCreatorLogo.png";
+import { TSCButton, TSCCard, StyledScrollbar } from "./components";
 import "./Home.css";
 import { ErrorCodes } from "./util/error-codes";
 import _ from "lodash";
 import { useTranslation } from "react-i18next";
+import { devSafeUrl } from "./util";
 
 const HeaderContainer = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+  justifySelf: "flex-start",
   gap: theme.spacing(2),
   marginBottom: theme.spacing(4)
 }));
@@ -27,16 +28,6 @@ const HeaderTitle = styled(Typography)(({ theme }) => ({
   fontSize: theme.typography.h2.fontSize
 }));
 
-const TSCOnlineHeader = () => {
-  const { t } = useTranslation();
-  return (
-    <HeaderContainer>
-      <TSCIcon src={TSCreatorLogo} alt="Logo" size="80px" marginTop="20px" />
-      <HeaderTitle variant="h2">{t("title.main")}</HeaderTitle>
-    </HeaderContainer>
-  );
-};
-
 export const Home = observer(function Home() {
   const { state, actions } = useContext(context);
   const theme = useTheme();
@@ -44,7 +35,24 @@ export const Home = observer(function Home() {
   const { t } = useTranslation();
   return (
     <div className="whole_page">
-      <TSCOnlineHeader />
+      <Box className="sub-header-section-landing-page">
+        <Box className="sub-header-section-landing-page-text">
+          <Typography variant="h2" fontWeight="700">
+            {"Welcome to TimeScale Creator!"}
+          </Typography>
+          <Typography className="sub-header-section-landing-page-description">
+            {
+              "TimeScale Creator is an advanced online cloud service designed to help you explore and visualize the geologic time scale with ease. With access to a vast internal database of over 20,000 global and regional events—including biologic, geomagnetic, sea-level, and stable isotope data—TimeScale Creator is the ultimate tool for researchers, educators, and enthusiasts of Earth history."
+            }
+          </Typography>
+        </Box>
+        <img
+          loading="lazy"
+          rel="preload"
+          className="sub-header-section-landing-page-image"
+          src={devSafeUrl("/public/website-images/landing-page.png")}
+        />
+      </Box>
       {Object.entries(state.presets).map(([type, configArray]) => {
         return <TSCPresetHighlights key={type} navigate={navigate} configArray={configArray} type={type} />;
       })}
