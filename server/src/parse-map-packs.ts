39474d63512f6ac8a63a1c85ac25047e1965d9c4
Changk820 <changk7973@gmail.com>
2025-01-23 21:46:18 -0500

    changing path traversal to see if security issue fixed

--- server/src/parse-map-packs.ts
+++ server/src/parse-map-packs.ts
@@ -113,7 +113,8 @@ export function processLine(
         );
       }
       map.name = formatColumnName(String(info[1]));
-      const trimmedPath = path.dirname(path.dirname(map_info));
+      // const trimmedPath = path.dirname(path.dirname(map_info));
+      const trimmedPath = map_info.replace(/\/map-packs\/.*$/, "");
       map.img = `/${trimmedPath}/MapImages/${String(info[2])}`;
       map.note = String(info[3]);
       break;
