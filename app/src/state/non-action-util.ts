95feb157010b72bc10290f8a9889b098695a178a
Jacqui <li4584@purdue.edu>
2025-01-23 19:24:34 -0500

    route for download workshop files

--- app/src/state/non-action-util.ts
+++ app/src/state/non-action-util.ts
@@ -4,13 +4,13 @@ import {
   DatapackUniqueIdentifier,
   MapInfo,
   SharedUser,
+  SharedWorkshop,
   isOfficialDatapack,
   isUserDatapack,
   isWorkshopDatapack
 } from "@tsconline/shared";
 import { devSafeUrl } from "../util";
 import dayjs from "dayjs";
-import { Workshop } from "../Workshops";
 import { State } from "./state";
 
 export function isMetadataLoading(skeletonStates: State["skeletonStates"]) {
@@ -124,19 +124,19 @@ export function hasLeadingTrailingWhiteSpace(input: string) {
   return input.trim() !== input;
 }
 
-export function getActiveWorkshops(workshops: Workshop[]) {
+export function getActiveWorkshops(workshops: SharedWorkshop[]) {
   const now = new Date();
   const activeWorkshops = workshops.filter(
     (workshop) => workshop.active && new Date(workshop.start) <= now && new Date(workshop.end) >= now
   );
   return activeWorkshops;
 }
-export function getUpcomingWorkshops(workshops: Workshop[]) {
+export function getUpcomingWorkshops(workshops: SharedWorkshop[]) {
   const now = new Date();
   const upcomingWorkshops = workshops.filter((workshop) => !workshop.active && new Date(workshop.start) > now);
   return upcomingWorkshops;
 }
-export function getPastWorkshops(workshops: Workshop[]) {
+export function getPastWorkshops(workshops: SharedWorkshop[]) {
   const now = new Date();
   const pastWorkshops = workshops.filter((workshop) => new Date(workshop.end) < now);
   return pastWorkshops;
