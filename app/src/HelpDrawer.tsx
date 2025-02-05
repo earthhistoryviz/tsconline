de676e3674be1e71842274625c3741a505bafb50
Joshua Ho <jyho@purdue.edu>
2025-02-04 19:32:16 -0500

    fixed the bug of event columns

--- app/src/HelpDrawer.tsx
+++ app/src/HelpDrawer.tsx
@@ -2,40 +2,7 @@ import { Link } from "react-router-dom";
 import { Collapse, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import { useState } from "react";
 import { ExpandLess, ExpandMore } from "@mui/icons-material";
-
-const links = [
-  { to: "/help/presets", label: "Presets" },
-  { to: "/help/datapacks", label: "Datapacks" },
-  {
-    to: "/help/chart",
-    label: "Chart",
-    children: [
-      { to: "/help/chart/what_is_a_chart", label: "What is a chart?" },
-      {
-        to: "/help/chart/column_variants",
-        label: "Column Variants",
-        children: [
-          { to: "/help/chart/column_variants/block_columns", label: "Block Column" },
-          { to: "/help/chart/column_variants/point_columns", label: "Point Column" },
-          {
-            to: "/help/chart/column_variants/event_columns/*",
-            label: "Event Column",
-            children: [
-              { to: "/help/chart/column_variants/event_columns/dual_column_comparison", label: "Dual Column Comparison" },
-              { to: "/help/chart/column_variants/event_columns/data_mining", label: "Data Mining" }
-            ]
-          },
-          { to: "/help/chart/column_variants/freehand_columns", label: "Freehand Column" }
-        ]
-      },
-      { to: "/help/chart/saving_a_chart", label: "Saving a Chart" }
-    ]
-  },
-  { to: "/help/settings", label: "Settings" },
-  { to: "/help/help", label: "Help" },
-  { to: "/help/workshops", label: "Workshops" },
-  { to: "/help/options", label: "Options" }
-];
+import { links } from "./HelpMenuLinks";
 
 function NavItem({ link }) {
   const [open, setOpen] = useState(false);
@@ -59,9 +26,11 @@ function NavItem({ link }) {
       {link.children && (
         <Collapse in={open} timeout="auto" unmountOnExit>
           <List component="div" disablePadding sx={{ pl: 4 }}>
+
             {link.children.map((child, index) => (
               <NavItem key={index} link={child} />
             ))}
+            
           </List>
         </Collapse>
       )}
