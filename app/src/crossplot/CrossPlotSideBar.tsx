3840f2bc4f4963562d91f59b567857f1b82bf05c
paologum <pgumasin@purdue.edu>
2025-02-09 00:44:02 -0500

    add the floating generate

--- app/src/crossplot/CrossPlotSideBar.tsx
+++ app/src/crossplot/CrossPlotSideBar.tsx
@@ -10,12 +10,15 @@ import { CrossPlotTimeSettings } from "../types";
 import { ColumnInfo } from "@tsconline/shared";
 import { useTranslation } from "react-i18next";
 import { FormLabel } from "react-bootstrap";
-import { CustomDivider, NotImplemented } from "../components";
+import { CustomDivider, NotImplemented, TSCButton } from "../components";
+import { useNavigate } from "react-router";
 
 export const CrossPlotSideBar = observer(
   forwardRef<HTMLDivElement, {}>((_, ref) => {
     const [tabIndex, setTabIndex] = useState(0);
     const [sidebarWidth, setSidebarWidth] = useState("300px"); // this is so the sidebar retains the width when resized
+    const { actions } = useContext(context);
+    const navigate = useNavigate();
     const theme = useTheme();
     useEffect(() => {
       if (typeof ref === "function") return;
@@ -77,17 +80,22 @@ export const CrossPlotSideBar = observer(
             );
           })}
         </Box>
-        <Box className={styles.tabContent}>{tabs[tabIndex].component}</Box>
+        <Box className={styles.tabContent}>
+          <TSCButton className={styles.generate} onClick={() => actions.compileAndSendCrossPlotChartRequest(navigate)}>
+            Generate Cross Plot
+          </TSCButton>
+          {tabs[tabIndex].component}
+        </Box>
       </Box>
     );
   })
 );
 
-const Time: React.FC = () => {
+const Time: React.FC = observer(() => {
   const { state, actions } = useContext(context);
   const { t } = useTranslation();
   return (
-    <Box display="flex" flexDirection="column" padding="30px" gap="30px">
+    <Box className={styles.timeComponent}>
       <CrossPlotTimeSettingsForm
         formLabel={t("crossPlot.time.xAxis")}
         disabled
@@ -106,7 +114,7 @@ const Time: React.FC = () => {
       />
     </Box>
   );
-};
+});
 
 type CrossPlotTimeProps = {
   settings: CrossPlotTimeSettings;
@@ -116,104 +124,97 @@ type CrossPlotTimeProps = {
   formLabel: string;
   disabled?: boolean;
 };
-const CrossPlotTimeSettingsForm: React.FC<CrossPlotTimeProps> = ({
-  column,
-  settings,
-  formLabel,
-  setTimeSettings,
-  setCrossPlotChart,
-  disabled
-}) => {
-  const { t } = useTranslation();
-  const { state } = useContext(context);
-  const checkAgeRange = () => settings && settings.topStageAge > settings.baseStageAge;
-  const pleaseSelectAUnit = t("crossPlot.time.select-unit");
-  return (
-    <Box display="flex" flexDirection="column" gap="15px">
-      <Box display="flex" flexDirection="row" justifyContent="space-between">
-        <Typography className={styles.timeLabel} fontWeight="700">
-          {formLabel}
-        </Typography>
-        <Typography className={styles.unitLabel} fontWeight="700">
-          {column?.units || t("crossPlot.time.no-unit-selected")}
-        </Typography>
-      </Box>
-      <Select
-        disabled={disabled || !column}
-        value={column?.units || 0}
-        onChange={(evt) => {
-          const col = state.settingsTabs.columns?.children.find((col) => col.units === evt.target.value);
-          if (!col) return;
-          setCrossPlotChart(col);
-        }}
-        className={styles.unitSelect}>
-        {state.settingsTabs.columns &&
-          Object.entries(state.settingsTabs.columns.children).map(([index, column]) => (
-            <MenuItem key={index} value={column.units}>
-              {`${column.name} (${column.units})`}
+const CrossPlotTimeSettingsForm: React.FC<CrossPlotTimeProps> = observer(
+  ({ column, settings, formLabel, setTimeSettings, setCrossPlotChart, disabled }) => {
+    const { t } = useTranslation();
+    const { state } = useContext(context);
+    const checkAgeRange = () => settings && settings.topStageAge > settings.baseStageAge;
+    const pleaseSelectAUnit = t("crossPlot.time.select-unit");
+    return (
+      <Box display="flex" flexDirection="column" gap="15px">
+        <Box display="flex" flexDirection="row" justifyContent="space-between">
+          <Typography className={styles.timeLabel} fontWeight="700">
+            {formLabel}
+          </Typography>
+          <Typography className={styles.unitLabel} fontWeight="700">
+            {column?.units || t("crossPlot.time.no-unit-selected")}
+          </Typography>
+        </Box>
+        <Select
+          disabled={disabled || !column}
+          value={column?.units || 0}
+          onChange={(evt) => {
+            const col = state.settingsTabs.columns?.children.find((col) => col.units === evt.target.value);
+            if (!col) return;
+            setCrossPlotChart(col);
+          }}
+          className={styles.unitSelect}>
+          {state.settingsTabs.columns &&
+            Object.entries(state.settingsTabs.columns.children).map(([index, column]) => (
+              <MenuItem key={index} value={column.units}>
+                {`${column.name} (${column.units})`}
+              </MenuItem>
+            ))}
+          {!column && (
+            <MenuItem value={0}>
+              <em>{t("crossPlot.time.no-chart-units-available")}</em>
             </MenuItem>
-          ))}
-        {!column && (
-          <MenuItem value={0}>
-            <em>{t("crossPlot.time.no-chart-units-available")}</em>
-          </MenuItem>
-        )}
-      </Select>
-      <Box display="flex" flexDirection="column" gap="15px" paddingLeft="13px">
-        <FormControl className={styles.timeIntervalForm}>
-          <FormLabel className={styles.timeIntervalLabel}>{t("settings.time.interval.top")}</FormLabel>
-          <TextField
-            size="small"
-            disabled={!column?.units}
-            label={`${column?.units || pleaseSelectAUnit}`}
-            type="number"
-            value={column?.units ? settings.topStageAge : ""}
-            onChange={(event) => {
-              if (!settings || !column?.units) return;
-              setTimeSettings({ topStageAge: parseFloat(event.target.value) });
-            }}
-            error={checkAgeRange()}
-            helperText={checkAgeRange() ? t("settings.time.interval.helper-text") : ""}
-            FormHelperTextProps={{ style: { fontSize: "13px" } }}
-          />
-        </FormControl>
-        <FormControl className={styles.timeIntervalForm}>
-          <FormLabel className={styles.timeIntervalLabel}>{t("settings.time.interval.base")}</FormLabel>
-          <TextField
-            size="small"
-            disabled={!column?.units}
-            label={`${column?.units || pleaseSelectAUnit}`}
-            type="number"
-            value={column?.units ? settings.baseStageAge : ""}
-            onChange={(event) => {
-              if (!settings || !column?.units) return;
-              setTimeSettings({ baseStageAge: parseFloat(event.target.value) });
-            }}
-            error={checkAgeRange()}
-            helperText={checkAgeRange() ? t("settings.time.interval.helper-text") : ""}
-            FormHelperTextProps={{ style: { fontSize: "13px" } }}
-          />
-        </FormControl>
-        <FormControl className={styles.timeIntervalForm}>
-          <FormLabel className={styles.timeIntervalLabel}>
-            {t("settings.time.interval.vertical-scale-header")}
-          </FormLabel>
-          <TextField
-            size="small"
-            disabled={!column?.units}
-            label={`${column?.units || pleaseSelectAUnit}`}
-            type="number"
-            value={column?.units ? settings.unitsPerMY : ""}
-            onChange={(event) => {
-              if (!settings || !column?.units) return;
-              setTimeSettings({ unitsPerMY: parseFloat(event.target.value) });
-            }}
-            error={checkAgeRange()}
-            helperText={checkAgeRange() ? t("settings.time.interval.helper-text") : ""}
-            FormHelperTextProps={{ style: { fontSize: "13px" } }}
-          />
-        </FormControl>
+          )}
+        </Select>
+        <Box display="flex" flexDirection="column" gap="15px" paddingLeft="13px">
+          <FormControl className={styles.timeIntervalForm}>
+            <FormLabel className={styles.timeIntervalLabel}>{t("settings.time.interval.top")}</FormLabel>
+            <TextField
+              size="small"
+              disabled={!column?.units}
+              label={`${column?.units || pleaseSelectAUnit}`}
+              type="number"
+              value={column?.units ? settings.topStageAge : ""}
+              onChange={(event) => {
+                if (!settings || !column?.units) return;
+                setTimeSettings({ topStageAge: parseFloat(event.target.value) });
+              }}
+              error={checkAgeRange()}
+              helperText={checkAgeRange() ? t("settings.time.interval.helper-text") : ""}
+              FormHelperTextProps={{ style: { fontSize: "13px" } }}
+            />
+          </FormControl>
+          <FormControl className={styles.timeIntervalForm}>
+            <FormLabel className={styles.timeIntervalLabel}>{t("settings.time.interval.base")}</FormLabel>
+            <TextField
+              size="small"
+              disabled={!column?.units}
+              label={`${column?.units || pleaseSelectAUnit}`}
+              type="number"
+              value={column?.units ? settings.baseStageAge : ""}
+              onChange={(event) => {
+                if (!settings || !column?.units) return;
+                setTimeSettings({ baseStageAge: parseFloat(event.target.value) });
+              }}
+              error={checkAgeRange()}
+              helperText={checkAgeRange() ? t("settings.time.interval.helper-text") : ""}
+              FormHelperTextProps={{ style: { fontSize: "13px" } }}
+            />
+          </FormControl>
+          <FormControl className={styles.timeIntervalForm}>
+            <FormLabel className={styles.timeIntervalLabel}>
+              {t("settings.time.interval.vertical-scale-header")}
+            </FormLabel>
+            <TextField
+              size="small"
+              disabled={!column?.units}
+              label={`${column?.units || pleaseSelectAUnit}`}
+              type="number"
+              value={column?.units ? settings.unitsPerMY : ""}
+              onChange={(event) => {
+                if (!settings || !column?.units) return;
+                setTimeSettings({ unitsPerMY: parseFloat(event.target.value) });
+              }}
+              FormHelperTextProps={{ style: { fontSize: "13px" } }}
+            />
+          </FormControl>
+        </Box>
       </Box>
-    </Box>
-  );
-};
+    );
+  }
+);
