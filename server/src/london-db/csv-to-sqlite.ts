99acf8c071efa0533e3a305866ff907bff5ac3e5
paologum <pgumasin@purdue.edu>
2025-02-09 23:24:31 -0500

    finished importing of database

--- server/src/london-db/csv-to-sqlite.ts
+++ server/src/london-db/csv-to-sqlite.ts
@@ -1,35 +1,57 @@
 import csv from "csv-parser";
 import { createReadStream } from "fs";
-import { LondonDatabaseKey, isLondonDatabaseType, londonDb, outputCSVDir } from "./london-database.js";
-import { readdir } from "fs/promises";
+import {
+  LondonDatabaseKey,
+  assertLondonDatabaseKey,
+  isLondonDatabaseType,
+  londonDb,
+  outputCSVDir,
+  toCamelCase
+} from "./london-database.js";
+import { readFile, readdir } from "fs/promises";
 import { join } from "path";
+import { createInterface } from "readline/promises";
 
 const BATCH_SIZE = 1;
 
-async function insertLargeCSVData(tableName: LondonDatabaseKey, filePath: string) {
-  const batch: Record<string, unknown>[] = [];
+const surroundedByQuotes = (value: string) => value.startsWith('"') && value.endsWith('"');
+
+async function insertLargeCSVData(tableName: string, filePath: string) {
+  const key = toCamelCase(tableName);
+  assertLondonDatabaseKey(key);
   return new Promise<void>((resolve, reject) => {
-    createReadStream(filePath)
+    let batch: Record<string, string | null | number>[] = [];
+    const stream = createReadStream(filePath)
       .pipe(csv())
-      .on("data", async (row: Record<string, unknown>) => {
-        row = Object.fromEntries(Object.entries(row).map(([key, value]) => [key, value === "" ? null : value]));
-        batch.push(row);
+      .on("data", async (row: Record<string, string>) => {
+        stream.pause();
+        const batchRow = Object.fromEntries(
+          Object.entries(row).map(([key, value]) => [
+            key,
+            value === "" ? null : surroundedByQuotes(value) ? value : !isNaN(Number(value)) ? Number(value) : value
+          ])
+        );
+        batch.push(batchRow);
         if (batch.length >= BATCH_SIZE) {
           try {
-            await londonDb.insertInto(tableName).values(batch).execute();
+            await londonDb.insertInto(key).values(batch).execute();
           } catch (error) {
             reject(error);
+            return;
+          } finally {
+            // reset batch
+            batch = [];
+            stream.resume();
           }
-          batch.pop();
-          batch.length = 0;
         }
       })
       .on("end", async () => {
         if (batch.length > 0) {
           try {
-            await londonDb.insertInto(tableName).values(batch).execute();
+            await londonDb.insertInto(key).values(batch).execute();
           } catch (error) {
             reject(error);
+            return;
           }
         }
         console.log(`Imported ${filePath} into ${tableName}`);
@@ -37,6 +59,22 @@ async function insertLargeCSVData(tableName: LondonDatabaseKey, filePath: string
       })
       .on("error", reject);
   });
+}
+
+async function insertLargeCSVDataReadFile(tableName: LondonDatabaseKey, filePath: string) {
+  const fileStream = createReadStream(filePath);
+  const readline = createInterface({ input: fileStream, crlfDelay: Infinity });
+  const headers = readline.line.split(",");
+
+  let batch: (string | null)[][] = [];
+  for await (const line of readline) {
+    const row = line.split(",").map((value) => (value === "" ? null : value));
+    batch.push(row);
+    if (batch.length >= BATCH_SIZE) {
+      await londonDb.insertInto(tableName).values(batch).execute();
+      batch = [];
+    }
+  }
 }
 async function getCSVFiles(directory: string): Promise<string[]> {
   try {
