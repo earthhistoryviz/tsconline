5ef14fd291daa4346dee9ee6994f9a11bca767da
paologum <pgumasin@purdue.edu>
2025-03-04 17:53:05 -0500

    incremental progress on crossplot

--- server/src/routes/crossplot-routes.ts
+++ server/src/routes/crossplot-routes.ts
@@ -1,7 +1,88 @@
 import { assertConvertCrossPlotRequest } from "@tsconline/shared";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { assetconfigs, verifyFilepath } from "../util.js";
+import { mkdir, readdir, writeFile } from "fs/promises";
+import md5 from "md5";
+import path from "path";
+import { getUploadedDatapackFilepath } from "../user/user-handler";
+import { spawn } from "child_process";
 
 export const convertCrossplot = async function convertCrossplot(request: FastifyRequest, reply: FastifyReply) {
   const body = request.body;
-  assertConvertCrossPlotRequest(body);
+  try {
+    assertConvertCrossPlotRequest(body);
+  } catch (error) {
+    reply.code(400).send({ message: "Incorrect request body for converting to crossplot" });
+    return;
+  }
+  // create a new directory for this request
+  let dir: string;
+  let outputTextFilepath: string;
+  let modelsTextFilepath: string;
+  try {
+    const hashedDir = md5(JSON.stringify(body));
+    dir = path.join(assetconfigs.modelConversionCacheDirectory, hashedDir);
+    await mkdir(dir, { recursive: true });
+    outputTextFilepath = path.join(dir, "output.txt");
+    if (await verifyFilepath(outputTextFilepath)) {
+      reply.code(400).send({ message: "Conversion already exists for this request", path: outputTextFilepath });
+      return;
+    }
+  } catch (error) {
+    reply.code(500).send({ message: "Error creating directory for this conversion" });
+    return;
+  }
+  try {
+    modelsTextFilepath = path.join(dir, "models.txt");
+    await writeFile(modelsTextFilepath, body.models);
+  } catch (e) {}
+  try {
+    const datapackFilepath = await getUploadedDatapackFilepath(body.uuid, body.datapackTitle);
+    const execJavaCommand = async (timeout: number) => {
+      const args = [
+        "-jar",
+        assetconfigs.activeJar,
+        "-node",
+        "-o",
+        outputTextFilepath,
+        "-convert",
+        datapackFilepath,
+        "-models",
+        modelsTextFilepath
+      ];
+      return new Promise<void>((resolve, reject) => {
+        const cmd = "java";
+        const javaProcess = spawn(cmd, args, { timeout, killSignal: "SIGKILL" });
+        let stdout = "";
+        let stderr = "";
+        let error = "";
+        javaProcess.stdout.on("data", (data) => {
+          stdout += data;
+        });
+
+        javaProcess.stderr.on("data", (data) => {
+          stderr += data;
+        });
+
+        javaProcess.on("error", (err) => {
+          error = err.message;
+        });
+
+        javaProcess.on("close", (code, signal) => {
+          if (signal == "SIGKILL") {
+            reject(new Error("Java process timed out"));
+            return;
+          }
+          console.log("Java finished, sending reply to browser");
+          console.log("Java error param: " + error);
+          console.log("Java stdout: " + stdout);
+          console.log("Java stderr: " + stderr);
+        });
+        resolve();
+      });
+    };
+  } catch (error) {
+    reply.code(500).send({ message: "Error converting to crossplot" });
+  }
+  reply.code(200).send({ message: "Conversion successful" });
 };
