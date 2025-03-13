a8c9d84b046a40c852043542fa8d91a1e7eed184
jay-jhk-lee <jaylee.62893@gmail.com>
2025-03-12 23:20:03 -0400

    refactor

--- app/src/settings_tabs/advanced_settings/OverlaySettings.tsx
+++ app/src/settings_tabs/advanced_settings/OverlaySettings.tsx
@@ -1,7 +1,7 @@
 import { context } from "../../state";
 import { ColumnInfo, assertEventSettings, assertPointSettings } from "@tsconline/shared";
 import { ColumnContainer, TSCCheckbox, Lottie, StyledScrollbar, CustomDivider, Accordion } from "../../components";
-import { Box, Tooltip, Typography, useTheme, IconButton } from "@mui/material";
+import { Box, Typography, useTheme, IconButton } from "@mui/material";
 import { useContext, useEffect, useRef, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { useTranslation } from "react-i18next";
@@ -10,15 +10,8 @@ import LightArrowUpIcon from "../../assets/icons/light-arrow-up.json";
 import MuiAccordionSummary from "@mui/material/AccordionSummary";
 import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
-import WarningAmberIcon from "@mui/icons-material/WarningAmber";
-
-import {
-  checkIfDataIsInRange,
-  checkIfDccColumn,
-  discardTscPrefix,
-  prependDualColCompColumnName
-} from "../../util/util";
-import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import { CustomTooltip } from "../../components";
+import { discardTscPrefix, prependDualColCompColumnName } from "../../util/util";
 import "./OverlaySettings.css";
 type OverlaySettingsProps = {
   column: ColumnInfo;
@@ -165,108 +158,43 @@ type ColumnAccordionProps = {
   column: ColumnInfo;
 };
 
-function checkIfDccDataIsInRange(dccColumn: ColumnInfo, userTopAge: number, userBaseAge: number) {
-  const { state } = useContext(context);
-  if (userBaseAge <= userTopAge) {
-    return false;
-  }
-  let reachedFirstRef = false;
-  while (!reachedFirstRef) {
-    if (
-      !(
-        (dccColumn.minAge <= userTopAge && dccColumn.maxAge >= userBaseAge) ||
-        (dccColumn.minAge > userTopAge && dccColumn.minAge < userBaseAge) ||
-        (dccColumn.maxAge < userBaseAge && dccColumn.maxAge > userTopAge)
-      )
-    ) {
-      return false;
-    }
-    if (dccColumn.columnDisplayType === "Event") {
-      assertEventSettings(dccColumn.columnSpecificSettings);
-    } else if (dccColumn.columnDisplayType === "Point") {
-      assertPointSettings(dccColumn.columnSpecificSettings);
-    } else {
-      console.warn("WARNING: dccColumn is not a valid column type");
-      return false;
-    }
-    //reached end of ref list
-    if (!dccColumn.columnSpecificSettings.dualColCompColumnRef) {
-      reachedFirstRef = true;
-      break;
-    }
-    const refCol = state.settingsTabs.columnHashMap.get(dccColumn.columnSpecificSettings.dualColCompColumnRef);
-    if (!refCol) {
-      console.log("WARNING: tried to get reference while checking dcc column, but is undefined");
-      return false;
-    }
-    dccColumn = refCol;
-  }
-  return true;
-}
-
-function OutOfRangeIcon() {
-  const theme = useTheme();
-  const { t } = useTranslation();
-  return (
-    <Tooltip
-      title={t("settings.column.tooltip.not-in-range")}
-      placement="top"
-      arrow
-      slotProps={{
-        popper: {
-          modifiers: [
-            {
-              name: "offset",
-              options: {
-                offset: [0, -10]
-              }
-            }
-          ]
-        }
-      }}>
-      <ErrorOutlineIcon
-        className="column-error-icon"
-        style={{
-          color: theme.palette.error.main
-        }}
-      />
-    </Tooltip>
-  );
-}
-
 const ColumnAccordion: React.FC<ColumnAccordionProps> = observer(({ column }) => {
   const { state } = useContext(context);
   const [expanded, setExpanded] = useState(false);
 
   const selectedClass = column.name === getSelectedOverlayColumn() ? "selected-column" : "";
 
-  const dataInRange = checkIfDccColumn(column)
-    ? checkIfDccDataIsInRange(
-        column,
-        state.settings.timeSettings[column.units].topStageAge,
-        state.settings.timeSettings[column.units].baseStageAge
-      )
-    : checkIfDataIsInRange(
-        column.minAge,
-        column.maxAge,
-        state.settings.timeSettings[column.units].topStageAge,
-        state.settings.timeSettings[column.units].baseStageAge
-      );
-
   //column can be chosen for overlay column
-  let validForOverlay = true ? column.columnDisplayType === "Event" || column.columnDisplayType === "Point" : false;
+  const validForOverlay = column.columnDisplayType == "Event" || column.columnDisplayType == "Point";
 
   // if there are no children, don't make an accordion
   if (column.children.length == 0) {
+    const ValidOverlay = () => (
+      <ColumnContainer
+        sx={{
+          cursor: "pointer"
+        }}
+        className="dcc-column-leaf">
+        <Typography className="column-display-name">{column.editName}</Typography>
+      </ColumnContainer>
+    );
+    const NotValidOverlay = () => (
+      <CustomTooltip title="Not an Event or Point Column" placement="left">
+        <ColumnContainer
+          sx={{
+            opacity: 1,
+            cursor: "default"
+          }}
+          className="dcc-column-leaf">
+          <Typography className="dcc-not-allowed">{column.editName}</Typography>
+        </ColumnContainer>
+      </CustomTooltip>
+    );
     return (
       <div
         className={`column-leaf-row-container ${selectedClass}`}
         onClick={() => {
-          if (
-            !validForOverlay ||
-            column.name === state.columnMenu.columnSelected ||
-            !state.columnMenu.columnSelected
-          ) {
+          if (!validForOverlay || column.name === state.columnMenu.columnSelected || !state.columnMenu.columnSelected) {
             return;
           }
           const refColumn = state.settingsTabs.columnHashMap.get(state.columnMenu.columnSelected);
@@ -284,16 +212,7 @@ const ColumnAccordion: React.FC<ColumnAccordionProps> = observer(({ column }) =>
             `class datastore.${column.columnDisplayType}Column:` + column.name;
         }}
         tabIndex={0}>
-        <ColumnContainer
-          sx={{
-            opacity: 1,
-            cursor: !validForOverlay ? "default" : "pointer"
-          }}
-          className="dcc-column-leaf">
-          <Typography className={!validForOverlay ? "dcc-not-allowed" : "column-display-name"}>
-            {column.editName}
-          </Typography>
-        </ColumnContainer>
+        {validForOverlay ? <ValidOverlay /> : <NotValidOverlay />}
       </div>
     );
   }
@@ -317,13 +236,7 @@ const ColumnAccordion: React.FC<ColumnAccordionProps> = observer(({ column }) =>
           }}
           aria-controls="panel-content"
           className={`column-accordion-summary ${selectedClass}`}>
-          <ColumnContainer
-            className="column-row-container"
-            sx={{
-              opacity: 1,
-              cursor: !validForOverlay ? "default" : ""
-            }}
-            onClick={() => setExpanded(!expanded)}>
+          <ColumnContainer className="column-row-container" onClick={() => setExpanded(!expanded)}>
             <Typography className="column-display-name">{column.editName}</Typography>
           </ColumnContainer>
         </MuiAccordionSummary>
