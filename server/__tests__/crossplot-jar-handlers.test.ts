144ae10c417e74837df364157da75f87ebc31247
paologum <pgumasin@purdue.edu>
2025-03-25 21:52:02 -0400

    add things

--- server/__tests__/crossplot-jar-handlers.test.ts
+++ server/__tests__/crossplot-jar-handlers.test.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import fsPromises from "fs/promises";
 import { vi, describe, it, expect, beforeEach, beforeAll } from "vitest";
-import child_process from "child_process";
+import * as child_process from "child_process";
 import * as util from "../src/util";
 import * as fetchUserFiles from "../src/user/fetch-user-files";
 import * as shared from "@tsconline/shared";
@@ -21,7 +21,6 @@ vi.mock("fs/promises", async (importOriginal) => {
     ...actual,
     readFile: vi.fn(actual.readFile),
     mkdir: vi.fn(actual.mkdir),
-    writeFile: vi.fn(() => {}),
     rm: vi.fn(actual.rm)
   };
 });
@@ -36,7 +35,31 @@ vi.mock("child_process", async (importOriginal) => {
     spawn: vi.fn(actual.spawn)
   };
 });
-const returnFilepaths = async (testCaseDir: string) => {
+vi.mock("../src/user/fetch-user-files", async () => {
+  return {
+    getDecryptedDatapackFilePath: vi.fn().mockResolvedValue("datapack/filepath")
+  };
+});
+vi.mock("../src/util", async (importOriginal) => {
+  const actual = await importOriginal<typeof util>();
+  return {
+    verifyFilepath: vi.fn(actual.verifyFilepath),
+    getActiveJar: vi.fn(() => "test.jar")
+  };
+});
+const getActiveJar = vi.spyOn(util, "getActiveJar");
+beforeAll(async () => {
+  vi.spyOn(console, "error").mockImplementation(() => undefined);
+  vi.spyOn(console, "log").mockImplementation(() => undefined);
+  const assetconfigs = JSON.parse(await fsPromises.readFile(path.join("server", "assets", "config.json"), "utf-8"));
+  assertAssetConfig(assetconfigs);
+  getActiveJar.mockImplementation(() => {
+    return isCI
+      ? "/home/runner/work/tsconline/tsconline/server/assets/jars/testUsageJar.jar"
+      : path.join("server", assetconfigs.activeJar);
+  });
+});
+const returnModelConversionFilepaths = (testCaseDir: string) => {
   const modelTextFilepath = path.join(testCaseDir, "models.txt");
   const settingsTextFilepath = path.join(testCaseDir, "settings.xml");
   const outputTextFilepath = path.join(testCaseDir, "output.txt");
@@ -46,7 +69,6 @@ const returnFilepaths = async (testCaseDir: string) => {
 const isCI = process.env.CI === "true" || !!process.env.GITHUB_ACTIONS;
 const conversionTestKeys = path.join("server", "__tests__", "__data__", "conversion-test-keys");
 describe("convertCrossPlotWithModelsInJar", async () => {
-  const getActiveJar = vi.spyOn(util, "getActiveJar");
   const spawn = vi.spyOn(child_process, "spawn");
   const getDecryptedDatapackFilePath = vi.spyOn(fetchUserFiles, "getDecryptedDatapackFilePath");
   const verifyFilepath = vi.spyOn(util, "verifyFilepath");
@@ -55,21 +77,12 @@ describe("convertCrossPlotWithModelsInJar", async () => {
     vi.clearAllMocks();
   });
   beforeAll(async () => {
-    vi.spyOn(console, "error").mockImplementation(() => undefined);
-    vi.spyOn(console, "log").mockImplementation(() => undefined);
     if (await checkFileExists(generatedOutputFileDirectory)) {
       await fsPromises.rm(generatedOutputFileDirectory, { recursive: true, force: true });
     }
     await fsPromises.mkdir(generatedOutputFileDirectory, { recursive: true });
-    const assetconfigs = JSON.parse(await fsPromises.readFile(path.join("server", "assets", "config.json"), "utf-8"));
-    assertAssetConfig(assetconfigs);
-    getActiveJar.mockImplementation(() => {
-      return isCI
-        ? "/home/runner/work/tsconline/tsconline/server/assets/jars/testUsageJar.jar"
-        : path.join("server", assetconfigs.activeJar);
-    });
   });
-  it("should return 500 if spawn fails", async () => {
+  it("should throw error if spawn fails", async () => {
     spawn.mockImplementationOnce(() => {
       throw new Error("Failed to spawn");
     });
@@ -87,14 +100,16 @@ describe("convertCrossPlotWithModelsInJar", async () => {
       )
     ).rejects.toThrow("Failed to spawn");
     expect(spawn).toHaveBeenCalledOnce();
+    expect(getActiveJar).toHaveBeenCalledOnce();
   });
-  it("should return 500 if bad datapack file", { timeout: 20000 }, async () => {
+  it("should return false if bad datapack file", { timeout: 20000 }, async () => {
     const testCase = "test-case-2";
     const testCaseFilepath = path.join(conversionTestKeys, testCase);
-    const { modelTextFilepath, settingsTextFilepath, datapackFilepath } = await returnFilepaths(testCaseFilepath);
+    const { modelTextFilepath, settingsTextFilepath, datapackFilepath } =
+      returnModelConversionFilepaths(testCaseFilepath);
     getDecryptedDatapackFilePath.mockResolvedValueOnce(datapackFilepath);
     const outputFileLocation = path.join(generatedOutputFileDirectory, `${testCase}-output.txt`);
-    await convertCrossPlotWithModelsInJar(
+    const response = await convertCrossPlotWithModelsInJar(
       [
         {
           title: "datapackTitle",
@@ -105,19 +120,20 @@ describe("convertCrossPlotWithModelsInJar", async () => {
       modelTextFilepath,
       settingsTextFilepath
     );
+    expect(response).toEqual(false);
     expect(getDecryptedDatapackFilePath).toHaveBeenCalledOnce();
     expect(verifyFilepath).toHaveBeenCalledOnce();
     expect(verifyFilepath).toHaveReturnedWith(false);
   });
-  it("should return 200 and file if successful", { timeout: 20000 }, async () => {
+  it("should return true and export output file if successful", { timeout: 20000 }, async () => {
     const testCase = "test-case-1";
     const testCaseFilepath = path.join(conversionTestKeys, testCase);
     const { datapackFilepath, modelTextFilepath, settingsTextFilepath, outputTextFilepath } =
-      await returnFilepaths(testCaseFilepath);
+      returnModelConversionFilepaths(testCaseFilepath);
     getDecryptedDatapackFilePath.mockResolvedValueOnce(datapackFilepath);
     const correctOutput = await fsPromises.readFile(outputTextFilepath);
     const outputFileLocation = path.join(generatedOutputFileDirectory, `${testCase}-output.txt`);
-    await convertCrossPlotWithModelsInJar(
+    const response = await convertCrossPlotWithModelsInJar(
       [
         {
           title: "datapackTitle",
@@ -128,6 +144,7 @@ describe("convertCrossPlotWithModelsInJar", async () => {
       modelTextFilepath,
       settingsTextFilepath
     );
+    expect(response).toEqual(true);
     const generatedOutput = await fsPromises.readFile(outputFileLocation);
     expect(getDecryptedDatapackFilePath).toHaveBeenCalledOnce();
     expect(spawn).toHaveBeenCalledOnce();
@@ -138,6 +155,18 @@ describe("convertCrossPlotWithModelsInJar", async () => {
     });
   });
 });
+const returnAutoPlotFilepaths = (testCaseDir: string) => {
+  const settingsTextFilepath = path.join(testCaseDir, "settings.xml");
+  const outputTextFilepath = path.join(testCaseDir, "output.txt");
+  const datapackOneFilepath = path.join(testCaseDir, "ma.txt");
+  const datapackTwoFilepath = path.join(testCaseDir, "feet.txt");
+  return {
+    settingsTextFilepath,
+    outputTextFilepath,
+    datapackOneFilepath,
+    datapackTwoFilepath
+  };
+};
 describe("autoPlotPointsWithJar", async () => {
   beforeEach(() => {
     vi.clearAllMocks();
@@ -145,25 +174,30 @@ describe("autoPlotPointsWithJar", async () => {
   const conversionTestKeys = path.join("server", "__tests__", "__data__", "auto-plot-test-keys");
   const generatedOutputFileDirectory = path.join(conversionTestKeys, "generated-file");
   beforeAll(async () => {
-    vi.spyOn(console, "error").mockImplementation(() => undefined);
-    vi.spyOn(console, "log").mockImplementation(() => undefined);
     if (await checkFileExists(generatedOutputFileDirectory)) {
       await fsPromises.rm(generatedOutputFileDirectory, { recursive: true, force: true });
     }
     await fsPromises.mkdir(generatedOutputFileDirectory, { recursive: true });
-    const assetconfigs = JSON.parse(await fsPromises.readFile(path.join("server", "assets", "config.json"), "utf-8"));
-    assertAssetConfig(assetconfigs);
-    getActiveJar.mockImplementation(() => {
-      return isCI
-        ? "/home/runner/work/tsconline/tsconline/server/assets/jars/testUsageJar.jar"
-        : path.join("server", assetconfigs.activeJar);
-    });
   });
   const getDecryptedDatapackFilePath = vi.spyOn(fetchUserFiles, "getDecryptedDatapackFilePath");
   const getUUIDOfDatapackType = vi.spyOn(shared, "getUUIDOfDatapackType");
-  const getActiveJar = vi.spyOn(util, "getActiveJar");
   const spawn = vi.spyOn(child_process, "spawn");
   const verifyFilepath = vi.spyOn(util, "verifyFilepath");
+  it("should return false if less than 2 datapacks", async () => {
+    const response = await autoPlotPointsWithJar(
+      [
+        {
+          title: "datapackTitle",
+          type: "official"
+        }
+      ],
+      "output",
+      "settings"
+    );
+    expect(response).toEqual(false);
+    expect(getDecryptedDatapackFilePath).toHaveBeenCalledOnce();
+    expect(getUUIDOfDatapackType).toHaveBeenCalledOnce();
+  });
   it("should throw an error if spawn fails", async () => {
     spawn.mockImplementationOnce(() => {
       throw new Error("Failed to spawn");
@@ -174,6 +208,10 @@ describe("autoPlotPointsWithJar", async () => {
           {
             title: "datapackTitle",
             type: "official"
+          },
+          {
+            title: "datapackTitle",
+            type: "official"
           }
         ],
         "output",
@@ -181,8 +219,68 @@ describe("autoPlotPointsWithJar", async () => {
       )
     ).rejects.toThrow("Failed to spawn");
     expect(spawn).toHaveBeenCalledOnce();
-    expect(getDecryptedDatapackFilePath).toHaveBeenCalledOnce();
-    expect(getUUIDOfDatapackType).toHaveBeenCalledOnce();
+    expect(getActiveJar).toHaveBeenCalledOnce();
+    expect(getDecryptedDatapackFilePath).toHaveBeenCalledTimes(2);
+    expect(getUUIDOfDatapackType).toHaveBeenCalledTimes(2);
   });
-  it("should return ");
+  it("should return false if bad datapack file", async () => {
+    const testCase = "bad-test-case-1";
+    const testCaseFilepath = path.join(conversionTestKeys, testCase);
+    const { datapackOneFilepath, datapackTwoFilepath, settingsTextFilepath } =
+      returnAutoPlotFilepaths(testCaseFilepath);
+    getDecryptedDatapackFilePath.mockResolvedValueOnce(datapackOneFilepath).mockResolvedValueOnce(datapackTwoFilepath);
+    const outputFileLocation = path.join(generatedOutputFileDirectory, `${testCase}-output.txt`);
+    const response = await autoPlotPointsWithJar(
+      [
+        {
+          title: "datapackTitle",
+          type: "official"
+        },
+        {
+          title: "datapackTitle",
+          type: "official"
+        }
+      ],
+      outputFileLocation,
+      settingsTextFilepath
+    );
+    expect(response).toEqual(false);
+    expect(verifyFilepath).toHaveBeenCalledOnce();
+    expect(verifyFilepath).toHaveReturnedWith(false);
+    expect(getDecryptedDatapackFilePath).toHaveBeenCalledTimes(2);
+    expect(getUUIDOfDatapackType).toHaveBeenCalledTimes(2);
+  });
+  it("should return true and correct output file if successful", async () => {
+    const testCase = "test-case-1";
+    const testCaseFilepath = path.join(conversionTestKeys, testCase);
+    const { datapackOneFilepath, datapackTwoFilepath, settingsTextFilepath, outputTextFilepath } =
+      returnAutoPlotFilepaths(testCaseFilepath);
+    getDecryptedDatapackFilePath.mockResolvedValueOnce(datapackOneFilepath).mockResolvedValueOnce(datapackTwoFilepath);
+    const outputFileLocation = path.join(generatedOutputFileDirectory, `${testCase}-output.txt`);
+    const response = await autoPlotPointsWithJar(
+      [
+        {
+          title: "datapackTitle",
+          type: "official"
+        },
+        {
+          title: "datapackTitle",
+          type: "official"
+        }
+      ],
+      outputFileLocation,
+      settingsTextFilepath
+    );
+    expect(response).toEqual(true);
+    const correctOutput = await fsPromises.readFile(outputTextFilepath);
+    const generatedOutput = await fsPromises.readFile(outputFileLocation);
+    expect(generatedOutput).toEqual(correctOutput);
+    expect(verifyFilepath).toHaveBeenCalledOnce();
+    expect(spawn).toHaveBeenCalledOnce();
+    expect(getDecryptedDatapackFilePath).toHaveBeenCalledTimes(2);
+    expect(getUUIDOfDatapackType).toHaveBeenCalledTimes(2);
+    await fsPromises.rm(outputFileLocation, { force: true }).catch(() => {
+      // eslint-disable-next-line no-console
+    });
+  });
 });
