3763b007800f8178ab5dbb0d24be51a84a41b169
Ellis Selznick <eselznic@purdue.edu>
2025-04-17 13:14:32 -0400

    removed auto webserver

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "yarn dev",
-    url: "http://localhost:5173",
-    reuseExistingServer: !process.env.CI
-  }
+  // webServer: {
+  //   command: "yarn dev",
+  //   url: "http://localhost:5173",
+  //   reuseExistingServer: !process.env.CI
+  // }
 });
