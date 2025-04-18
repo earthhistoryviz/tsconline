3c87324eaf82fd620e6c061ea50e05ee0dfce060
Ellis Selznick <eselznic@purdue.edu>
2025-04-17 23:13:54 -0400

    added back yarn dev to config

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "yarn start",
+    command: "yarn dev",
     url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI
   }
