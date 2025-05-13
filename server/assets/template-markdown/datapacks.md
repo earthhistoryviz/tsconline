e4f3a98d5320f1d479458f8f186987d64ab18014
paologum <pgumasin@purdue.edu>
2025-05-12 23:42:43 -0400

    add markdown template and processing initial commit

---
+++ server/assets/template-markdown/datapacks.md
@@ -0,0 +1,7 @@
+---
+title: "Datapacks"
+authoredBy: "admin"
+pathname: "datapacks"
+lastUpdated: "2025-05-12 15:32:00"
+---
+datpaacks
---
+++ server/src/help/process-markdown-tree.ts
@@ -0,0 +1,37 @@
+import { readFile, readdir } from "fs/promises";
+import { assetconfigs, loadAssetConfigs, verifyFilepath } from "../util.js"
+import matter from "gray-matter";
+import { MarkdownTree, assertMarkdownFileMetadata } from "@tsconline/shared";
+export const processMarkdownTree = async () => {
+    await loadAssetConfigs();
+    const markdownDirectory = assetconfigs.helpDirectory;
+    if (!await verifyFilepath(markdownDirectory)) {
+        throw new Error("Markdown directory is not a valid path");
+    }
+    const tree: MarkdownTree = {};
+    const addToTree = async (path: string, tree: MarkdownTree, prefix?: string) => {
+        const files = await readdir(path, { withFileTypes: true });
+        for (const file of files) {
+            const filePath = `${path}/${file.name}`;
+            if (file.isDirectory()) {
+                const child: MarkdownTree = {};
+                tree[file.name] = child;
+                await addToTree(filePath, child);
+            } else if (file.isFile() && file.name.endsWith(".md")) {
+                const content = await readFile(filePath, "utf-8");
+                const parsed = matter(content);
+                const { data, content: body} = parsed;
+                assertMarkdownFileMetadata(data);
+                console.log("path: ", filePath);
+                tree[data.pathname] = {
+                    ...data,
+                    markdown: body
+                };
+            } else {
+                throw new Error(`File ${filePath} is not a markdown file or directory`);
+            }
+        }
+    }
+    await addToTree(markdownDirectory, tree);
+    return tree;
+}
