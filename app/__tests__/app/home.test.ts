24f25ae33c45b57b4d3ec242569d0fdabfa56427
Ellis Selznick <eselznic@purdue.edu>
2025-05-19 02:05:22 +0000

    improved tests and workflow

--- app/__tests__/app/home.test.ts
+++ app/__tests__/app/home.test.ts
@@ -1,7 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-test("navigation to presets page works", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:5173");
+  await page.waitForTimeout(2000); // Wait for 2 seconds to ensure the page is fully loaded
+}
+);
+
+test("navigation to presets page works", async ({ page }) => {
   const datapacksTab = page.locator(".qsg-presets");
   await expect(datapacksTab).toBeVisible();
   await datapacksTab.click();
@@ -9,7 +14,6 @@ test("navigation to presets page works", async ({ page }) => {
 });
 
 test("navigation to datapack page works", async ({ page }) => {
-  await page.goto("http://localhost:5173");
   const datapacksTab = page.locator(".qsg-chart");
   await expect(datapacksTab).toBeVisible();
   await datapacksTab.click();
@@ -17,7 +21,6 @@ test("navigation to datapack page works", async ({ page }) => {
 });
 
 test("navigation to settings page works", async ({ page }) => {
-  await page.goto("http://localhost:5173");
   const datapacksTab = page.locator(".qsg-settings");
   await expect(datapacksTab).toBeVisible();
   await datapacksTab.click();
@@ -25,7 +28,6 @@ test("navigation to settings page works", async ({ page }) => {
 });
 
 test("navigation to help page works", async ({ page }) => {
-  await page.goto("http://localhost:5173");
   const datapacksTab = page.locator(".qsg-help");
   await expect(datapacksTab).toBeVisible();
   await datapacksTab.click();
@@ -33,7 +35,6 @@ test("navigation to help page works", async ({ page }) => {
 });
 
 test("navigation to workshops page works", async ({ page }) => {
-  await page.goto("http://localhost:5173");
   const datapacksTab = page.locator(".qsg-workshops");
   await expect(datapacksTab).toBeVisible();
   await datapacksTab.click();
@@ -41,9 +42,37 @@ test("navigation to workshops page works", async ({ page }) => {
 });
 
 test("navigation to about page works", async ({ page }) => {
-  await page.goto("http://localhost:5173");
   const datapacksTab = page.locator(".qsg-about");
   await expect(datapacksTab).toBeVisible();
   await datapacksTab.click();
   await expect(page).toHaveURL(/.*\/about/);
+});
+
+test("login is functional", async ({ page }) => {
+  const loginButton = page.locator("text=SIGN IN");
+  await expect(loginButton).toBeVisible();
+  await expect(loginButton).toBeEnabled();
+  await loginButton.click();
+
+  const acceptButton = page.locator("text=Accept");
+  await expect(acceptButton).toBeVisible();
+  await acceptButton.click();
+
+  await expect(page).toHaveURL(/.*\/login/);
+
+  const usernameInput = page.locator('#username');
+  const passwordInput = page.locator('#password');
+  const signInButton = page.locator('[data-testid="LoginIcon"]');
+
+  await expect(usernameInput).toBeVisible();
+  await expect(usernameInput).toBeEnabled();
+  await usernameInput.fill("admin");
+
+  await expect(passwordInput).toBeVisible();
+  await expect(passwordInput).toBeEnabled();
+  await passwordInput.fill("admin-password");
+
+  await expect(signInButton).toBeVisible();
+  await expect(signInButton).toBeEnabled();
+  await signInButton.click();
 });
