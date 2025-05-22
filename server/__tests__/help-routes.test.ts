90c9b0102503dd7fce7894c6c39111a639f7e441
paologum <pgumasin@purdue.edu>
2025-05-22 15:04:43 -0400

    quality for tests

--- server/__tests__/help-routes.test.ts
+++ server/__tests__/help-routes.test.ts
@@ -5,46 +5,46 @@ import fastify, { FastifyInstance } from "fastify";
 import * as processMarkdownTree from "../src/help/process-markdown-tree";
 import { MarkdownTree } from "@tsconline/shared";
 vi.mock("../src/help/process-markdown-tree", () => ({
-    processMarkdownTree: vi.fn().mockResolvedValue({
-        title: "Test Title",
-    }),
+  processMarkdownTree: vi.fn().mockResolvedValue({
+    title: "Test Title"
+  })
 }));
 
 let app: FastifyInstance;
 beforeAll(async () => {
-    app = fastify();
-    app.get("/fetch-markdown-files", fetchMarkdownFiles);
-    await app.listen({ host: "", port: 1839 });
-})
+  app = fastify();
+  app.get("/fetch-markdown-files", fetchMarkdownFiles);
+  await app.listen({ host: "", port: 1839 });
+});
 
 afterAll(async () => {
-    await app.close();
+  await app.close();
 });
 
 describe("fetch markdown files", () => {
-    const processMarkdownTreeSpy = vi.spyOn(processMarkdownTree, "processMarkdownTree");
-    it("should return 500 if error occurs", async () => {
-        processMarkdownTreeSpy.mockRejectedValue(new Error("Test error"));
-        const response = await app.inject({
-            method: "GET",
-            url: "/fetch-markdown-files",
-        });
-        expect(response.statusCode).toBe(500);
-        expect(response.json()).toEqual({ error: "Failed to fetch markdown files" });
+  const processMarkdownTreeSpy = vi.spyOn(processMarkdownTree, "processMarkdownTree");
+  it("should return 500 if error occurs", async () => {
+    processMarkdownTreeSpy.mockRejectedValue(new Error("Test error"));
+    const response = await app.inject({
+      method: "GET",
+      url: "/fetch-markdown-files"
     });
-    it ("should return 200 and the markdown tree", async () => {
-        processMarkdownTreeSpy.mockResolvedValue({
-            title: "Test Title",
-            content: "Test Content",
-        } as unknown as MarkdownTree);
-        const response = await app.inject({
-            method: "GET",
-            url: "/fetch-markdown-files",
-        });
-        expect(response.statusCode).toBe(200);
-        expect(response.json()).toEqual({
-            title: "Test Title",
-            content: "Test Content",
-        });
+    expect(response.statusCode).toBe(500);
+    expect(response.json()).toEqual({ error: "Failed to fetch markdown files" });
+  });
+  it("should return 200 and the markdown tree", async () => {
+    processMarkdownTreeSpy.mockResolvedValue({
+      title: "Test Title",
+      content: "Test Content"
+    } as unknown as MarkdownTree);
+    const response = await app.inject({
+      method: "GET",
+      url: "/fetch-markdown-files"
     });
+    expect(response.statusCode).toBe(200);
+    expect(response.json()).toEqual({
+      title: "Test Title",
+      content: "Test Content"
+    });
+  });
 });
