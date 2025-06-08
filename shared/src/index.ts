39546381975652bfc9057c513852bc005f181c4b
Leyton <leytonbostre@gmail.com>
2025-06-08 15:04:13 -0700

    pr fixes

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -841,7 +841,7 @@ export function assertCommentType(o: any): asserts o is CommentType {
     throwError("CommentType", "flagged", "number", o.flagged);
   if (typeof o.username !== "string") throwError("CommentType", "username", "string", o.username);
   if (o.pictureUrl && typeof o.pictureUrl !== "string") throwError("CommentType", "pictureUrl", "string", o.pictureUrl);
-  if (o.isSelf && typeof o.isSelf !== "boolean") throwError("CommentType", "isSelf", "boolean", o.isSelf);
+  if (o.isSelf !== undefined && typeof o.isSelf !== "boolean") throwError("CommentType", "isSelf", "boolean", o.isSelf);
 }
 
 export function convertDatapackConfigForChartRequestToUniqueDatapackIdentifier(
