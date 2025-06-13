d92dcf451880f7b200b079dfde9a943a9980b183
jay-jhk-lee <jaylee.62893@gmail.com>
2025-06-13 11:22:51 -0400

    directory change

--- server/src/translations/remove-extra-translations.ts
+++ server/src/translations/remove-extra-translations.ts
@@ -2,6 +2,7 @@
 import { readFileSync, writeFileSync } from "fs";
 import { assetconfigs, loadAssetConfigs } from "../util.js";
 import path from "path";
+import chalk from "chalk";
 
 const availLanguages = path.join("..", "shared", "translations", "available-languages.json");
 const engTranslation = path.join("..", "shared", "translations", "en.json");
@@ -14,14 +15,18 @@ try {
     if (value === "en") continue;
     const langTranslationJSON = path.join("..", "shared", "translations", `${value}.json`);
     const langData = JSON.parse(readFileSync(langTranslationJSON, "utf-8"));
-    const prunedData = removeExtraKeys(langData, engData);
+    const [prunedData, didRemove] = removeExtraKeys(langData, engData);
     writeFileSync(langTranslationJSON, JSON.stringify(prunedData, null, 4), "utf-8");
 
     const langCSV = path.join(assetconfigs.translationsDirectory, `${value}.csv`);
     const flattened = flattenJson(prunedData);
     const csvContent = flattened.map(([key, value]) => `${key},${value}`).join("\n");
     writeFileSync(langCSV, csvContent, "utf-8");
+    if (didRemove) {
+      console.log(chalk.green(`removed extra translations for ${value}`));
+    }
   }
+  console.log(chalk.green("successfully removed extra translations!"));
 } catch (e) {
   console.log("failed to remove translations: ", e);
 }
@@ -30,17 +35,19 @@ interface JSONObject {
   [key: string]: JSONValue;
 }
 //other and eng are on the same level
-function removeExtraKeys(lang: JSONObject, eng: JSONObject) {
+function removeExtraKeys(lang: JSONObject, eng: JSONObject): [JSONObject, boolean] {
+  let didRemove = false;
   for (const key of Object.keys(lang)) {
     const langValue = lang[key];
     const engValue = eng[key];
     if (engValue === undefined) {
       delete lang[key];
+      didRemove = true;
     } else if (isObject(langValue!) && isObject(engValue)) {
       removeExtraKeys(langValue, engValue);
     }
   }
-  return lang;
+  return [lang, didRemove];
 }
 function isObject(value: JSONValue): value is JSONObject {
   return typeof value === "object" && value !== null && !Array.isArray(value);
