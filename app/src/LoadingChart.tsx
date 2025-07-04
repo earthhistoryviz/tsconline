3c3d0d985da3f7c33592ae77a6dec2e7c8b7b5b1
asivath <sivathanua27@gmail.com>
2025-07-03 21:50:17 -0400

    sync packages and remove lottie from loading chart

--- app/src/LoadingChart.tsx
+++ app/src/LoadingChart.tsx
@@ -1,12 +1,11 @@
 import { Box, Typography, styled } from "@mui/material";
-import { Lottie } from "./components";
-import loader from "./assets/icons/loading.json";
 import { useTranslation } from "react-i18next";
 import LinearProgress from "@mui/material/LinearProgress";
 
 const BorderLinearProgress = styled(LinearProgress)(() => ({
   height: 10,
-  borderRadius: 5
+  borderRadius: 5,
+  width: "30%"
 }));
 
 type LoadingChartProps = {
@@ -17,20 +16,12 @@ const LoadingChart = ({ percent, stage }: LoadingChartProps) => {
   const { t } = useTranslation();
   return (
     <Box bgcolor="background.main" className="loading-container">
-      <div>
-        <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center">
-          <Lottie animationData={loader} autoplay loop width={200} height={200} speed={0.7} />
-          <div>
-            <Typography variant="h1" className="loading">
-              {" "}
-              {t("loading.loading-chart")}
-            </Typography>
-            <Typography className="loading-sub"> ( {t("loading.time")})</Typography>
-          </div>
-        </Box>
-        <Typography>{stage}</Typography>
-        <BorderLinearProgress variant="determinate" value={percent} />
-      </div>
+      <Typography variant="h1" className="loading">
+        {t("loading.loading-chart")}
+      </Typography>
+      <Typography className="loading-sub">({t("loading.time")})</Typography>
+      <Typography mt={5}>{stage}</Typography>
+      <BorderLinearProgress variant="determinate" value={percent} />
     </Box>
   );
 };
