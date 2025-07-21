b49040c86b84c73e14d6cad48a918a8f85fb9173
Neel <Neelptl2015@gmail.com>
2025-07-20 21:15:31 -0500

    Retrying push to origin

--- server/src/workshop/workshop-auth.ts
+++ server/src/workshop/workshop-auth.ts
@@ -114,6 +114,7 @@ export const workshopRoutes = async (fastify: FastifyInstance, _options: Registe
     },
     serveWorkshopHyperlinks
   );
+  
   fastify.get(
     "/workshop-files/:workshopId/:fileName",
     {
