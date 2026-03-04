947a0ebebdc64162b62dd26e296659addf737b05
Neel <Neelptl2015@gmail.com>
2026-03-04 12:12:27 -0500

    Fixed /mcp check so no front end conflict

--- server/src/index.ts
+++ server/src/index.ts
@@ -83,7 +83,7 @@ const ipSet = new Set<string>();
 
 //IP restrict access to mcp routes
 server.addHook("onRequest", async (request: FastifyRequest, reply: FastifyReply) => {
-  if (request.url.startsWith("/mcp")) {
+  if (request.url.startsWith("/mcp/")) {
     const allowedIPs = ["127.0.0.1", "::1"];
     if (!allowedIPs.includes(request.ip)) {
       reply.status(401).send({ error: "Unauthorized access" });
